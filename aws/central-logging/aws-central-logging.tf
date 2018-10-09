84e9fc88ccd92541144cb6721087d051a6730d54
Tyson Beck <tyson.beck@gravicore.io>
2018-10-09 15:57:27 -0600

    Cleaned up old code and corrected log path

--- aws/central-logging/aws-central-logging.tf
+++ aws/central-logging/aws-central-logging.tf
@@ -1,14 +1,3 @@
-# resource "aws_cloudformation_stack" "aws_central_log_bucket" {
-#   name         = "aws-central-log-bucket"
-#   capabilities = ["CAPABILITY_IAM"]
-
-#   parameters {
-#     BucketName = "msn-central-logging-bucket"
-#   }
-
-#   template_body = "${file("../terraform-central-logging/cloudformation/aws-central-log-bucket.cft")}"
-# }
-
 module "log_storage" {
   source    = "git::https://github.com/cloudposse/terraform-aws-s3-log-storage.git?ref=master"
   name      = "${var.name}"
@@ -34,36 +23,11 @@ resource "aws_cloudformation_stack" "aws_central_logging_destination" {
   capabilities = ["CAPABILITY_IAM"]
 
   parameters {
-    # LogBucketName       = "${aws_cloudformation_stack.aws_central_log_bucket.outputs["CentralLogBucket"]}"
     LogBucketName       = "${module.log_storage.bucket_id}"
-    LogS3Location       = "${var.s3_log_Location}/${element(values(var.child_account), count.index)}/${var.stage}/"
+    LogS3Location       = "${var.s3_log_Location}/${element(values(var.child_account), count.index)}/"
     ProcessingLambdaARN = "${aws_cloudformation_stack.aws_central_logging_lambda.outputs["Function"]}"
     SourceAccount       = "${element(keys(var.child_account), count.index)}"
   }
 
   template_body = "${file("${path.module}/cloudformation/aws-central-logging-destination.cft")}"
 }
-
-# resource "aws_cloudformation_stack" "aws_central_logging_destination2" {
-#   depends_on = [
-#     "module.log_storage",
-#     "aws_cloudformation_stack.aws_central_logging_lambda",
-#   ]
-
-
-#   name         = "${var.name}-552324202284"
-#   capabilities = ["CAPABILITY_IAM"]
-
-
-#   parameters {
-#     # LogBucketName       = "${aws_cloudformation_stack.aws_central_log_bucket.outputs["CentralLogBucket"]}"
-#     LogBucketName       = "${module.log_storage.bucket_id}"
-#     LogS3Location       = "log/test/"
-#     ProcessingLambdaARN = "${aws_cloudformation_stack.aws_central_logging_lambda.outputs["Function"]}"
-#     SourceAccount       = "552324202284"
-#   }
-
-
-#   template_body = "${file("${path.module}/cloudformation/aws-central-logging-destination.cft")}"
-# }
-
