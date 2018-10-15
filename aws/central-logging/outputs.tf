cc14eb342c368b572906d572cc0738c05bc5bfb5
Tyson Beck <tyson.beck@gravicore.io>
2018-10-15 16:17:19 -0600

    Corrected error with invalid variable.

--- aws/central-logging/outputs.tf
+++ aws/central-logging/outputs.tf
@@ -7,8 +7,3 @@ output "logging-lambda" {
   value       = "${aws_cloudformation_stack.aws_central_logging_lambda.outputs["Function"]}"
   description = "ARN of the central logging lambda"
 }
-
-output "vpc_flow_id" {
-  value       = "${aws_flow_log.vpc.id}"
-  description = "Flow Log IDs of VPCs"
-}
