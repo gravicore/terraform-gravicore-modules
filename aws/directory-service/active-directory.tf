e1f081822bd1c03b7bd64d5346f7912699c22e2f
Tyson Beck <tyson.beck@gravicore.io>
2018-10-16 14:54:54 -0600

    Directory Service initial commit

---
+++ aws/directory-service/active-directory.tf
@@ -0,0 +1,30 @@
+resource "aws_directory_service_directory" "default" {
+  name       = "${var.environment}.${var.parent_domain_name}"
+  password   = "${var.password}"
+  edition    = "${var.edition}"
+  type       = "${var.type}"
+  alias      = "${module.common_label.id}"
+  enable_sso = "${var.enable_sso}"
+  short_name = "${var.directory_services_short_name}"
+
+  vpc_settings {
+    vpc_id     = "${data.terraform_remote_state.vpc.vpc_id}"
+    subnet_ids = ["${data.terraform_remote_state.vpc.private_subnets}"]
+  }
+
+  tags = "${local.default_tags}"
+}
+
+resource "aws_vpc_dhcp_options" "default" {
+  domain_name          = "${aws_directory_service_directory.default.name}"
+  domain_name_servers  = ["${aws_directory_service_directory.default.dns_ip_addresses}"]
+  ntp_servers          = ["${aws_directory_service_directory.default.dns_ip_addresses}"]
+  netbios_name_servers = ["${aws_directory_service_directory.default.dns_ip_addresses}"]
+
+  tags = "${local.default_tags}"
+}
+
+resource "aws_vpc_dhcp_options_association" "default" {
+  vpc_id          = "${data.terraform_remote_state.vpc.vpc_id}"
+  dhcp_options_id = "${aws_vpc_dhcp_options.default.id}"
+}
---
+++ aws/directory-service/variables.tf
@@ -0,0 +1,50 @@
+variable "vpc_id" {
+  type        = "string"
+  description = "The fully qualified name for the directory, such as corp.example.com"
+}
+
+variable "subnet_ids" {
+  type        = "list"
+  default     = []
+  description = "The identifiers of the subnets for the directory servers (2 subnets in 2 different AZs)"
+}
+
+variable "name" {
+  type        = "string"
+  description = "The fully qualified name for the directory, such as corp.example.com"
+}
+
+variable "password" {
+  type        = "string"
+  description = "The fully qualified name for the directory, such as corp.example.com"
+}
+
+variable "edition" {
+  type        = "string"
+  default     = "Standard"
+  description = "The MicrosoftAD edition (Standard or Enterprise). Defaults to Enterprise (applies to MicrosoftAD type only)"
+}
+
+variable "type" {
+  type        = "string"
+  default     = "MicrosoftAD"
+  description = "The directory type (SimpleAD, ADConnector or MicrosoftAD are accepted values)"
+}
+
+variable "enable_sso" {
+  type        = "string"
+  default     = "false"
+  description = "Whether to enable single-sign on for the directory. Requires alias."
+}
+
+variable "alias" {
+  type        = "string"
+  default     = ""
+  description = "The alias for the directory (must be unique amongst all aliases in AWS). Required for enable_sso"
+}
+
+variable "short_name" {
+  type        = "string"
+  default     = ""
+  description = "The short name of the directory, such as CORP."
+}
