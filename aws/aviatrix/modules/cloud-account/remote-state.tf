8a4f3274822dfba48784c336794d1669f89efd34
Jeremy Odle <jeremy.odle@gravicore.io>
2018-10-25 21:51:07 -0600

    #9 Hardcoded Aviatrix role names

--- aws/aviatrix/modules/cloud-account/remote-state.tf
+++ aws/aviatrix/modules/cloud-account/remote-state.tf
@@ -1,29 +1,3 @@
-data "terraform_remote_state" "master_account" {
-  backend = "s3"
-
-  config {
-    region         = "${var.aws_region}"
-    bucket         = "${var.namespace}-master-prd-tf-state-${var.master_account_id}"
-    encrypt        = true
-    key            = "master/prd/acct/terraform.tfstate"
-    dynamodb_table = "${var.namespace}-master-prd-tf-state-lock"
-    role_arn       = "arn:aws:iam::${var.master_account_id}:role/grv_deploy_svc"
-  }
-}
-
-data "terraform_remote_state" "account" {
-  backend = "s3"
-
-  config {
-    region         = "${var.aws_region}"
-    bucket         = "${var.namespace}-master-prd-tf-state-${var.master_account_id}"
-    encrypt        = true
-    key            = "${var.stage}/${var.environment}/acct/terraform.tfstate"
-    dynamodb_table = "${var.namespace}-master-prd-tf-state-lock"
-    role_arn       = "arn:aws:iam::${var.master_account_id}:role/grv_deploy_svc"
-  }
-}
-
 data "terraform_remote_state" "aviatrix_controller" {
   backend = "s3"
 
