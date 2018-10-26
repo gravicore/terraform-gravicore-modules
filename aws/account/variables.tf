0d3b70476a8bd181365e8c1b74cd6a8aeed75cbb
Jeremy Odle <jeremy.odle@gravicore.io>
2018-10-25 18:56:06 -0600

    #9 Added standard labeling, added Aviatrix IAM role

--- aws/account/variables.tf
+++ aws/account/variables.tf
@@ -1,7 +1,50 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Platform Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
 variable "tags" {
   description = "https://aws.amazon.com/answers/account-management/aws-tagging-strategies/"
   default     = {}
 }
+
+variable "namespace" {
+  default = "grv"
+}
+
+variable "environment" {
+  default = "master"
+}
+
+variable "stage" {
+  default = "dev"
+}
+
+variable "repository" {
+  default = ""
+}
+
+variable "master_account_id" {}
+variable "account_id" {}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "name" {
+  default = "acct"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable terraform_module {
+  default = "github.com/gravicore/terraform-gravicore-modules/aws/account"
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Custom Variables
+# ----------------------------------------------------------------------------------------------------------------------
 
 variable allow_gravicore_access {
   description = "Flag to establish SAML connectivity for Gravicore managed services"
@@ -14,8 +57,4 @@ variable trusted_entity_account_id {
 
 variable "iam_account_alias" {
   description = "The account alias to create."
-}
-
-variable "aws_region" {
-  default = "us-east-1"
 }
