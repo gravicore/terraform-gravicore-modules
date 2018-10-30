a6107b7142c65c89a89a4bfecaceb0818dcd3de8
Jeremy Odle <jeremy.odle@gravicore.io>
2018-10-30 01:13:22 -0600

    #9 Updated aviatrix controller remote state reference

--- aws/aviatrix/modules/cloud-account/remote-state.tf
+++ aws/aviatrix/modules/cloud-account/remote-state.tf
@@ -5,7 +5,7 @@ data "terraform_remote_state" "aviatrix_controller" {
     region         = "${var.aws_region}"
     bucket         = "${var.namespace}-master-prd-tf-state-${var.master_account_id}"
     encrypt        = true
-    key            = "master/prd/aviatrix-controller/terraform.tfstate"
+    key            = "master/prd/avtx-cont/terraform.tfstate"
     dynamodb_table = "${var.namespace}-master-prd-tf-state-lock"
     role_arn       = "arn:aws:iam::${var.master_account_id}:role/grv_deploy_svc"
   }
