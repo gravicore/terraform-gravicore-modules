d48bcd6ebd7302a159ed4f44b2cbe4b4f4347c63
Jeremy Odle <jeremy.odle@gravicore.io>
2018-11-01 11:40:43 -0600

    #9 Default VPC to not map public IPs by default

--- aws/vpc/resources.tf
+++ aws/vpc/resources.tf
@@ -9,12 +9,13 @@ module "vpc" {
   name   = "${local.name_prefix}"
   tags   = "${local.module_vpc_tags}"
 
-  azs                = ["${var.aws_region}a", "${var.aws_region}b"]
-  cidr               = "${var.cidr_network}.0.0/16"
-  private_subnets    = ["${var.cidr_network}.0.0/19", "${var.cidr_network}.32.0/19"]
-  public_subnets     = ["${var.cidr_network}.128.0/20", "${var.cidr_network}.144.0/20"]
-  enable_nat_gateway = true
-  single_nat_gateway = false
+  azs                     = ["${var.aws_region}a", "${var.aws_region}b"]
+  cidr                    = "${var.cidr_network}.0.0/16"
+  private_subnets         = ["${var.cidr_network}.0.0/19", "${var.cidr_network}.32.0/19"]
+  public_subnets          = ["${var.cidr_network}.128.0/20", "${var.cidr_network}.144.0/20"]
+  enable_nat_gateway      = true
+  single_nat_gateway      = false
+  map_public_ip_on_launch = false
 }
 
 # Create a default key/pair for public and private instances
