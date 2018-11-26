296c9ea1ebaae745f8dcaa361a91ee447764b1a1
Jeremy Odle <jeremy.odle@gravicore.io>
2018-11-25 23:26:52 -0700

    #4 Renamed to spoke-vpc, removed NAT Gateways for Aviatrix Egress, updated TerraformModules, added key-pair submodule, changed ssm param key to Chamber default

---
+++ aws/spoke-vpc/module.tf
@@ -0,0 +1,131 @@
+terraform {
+  required_version = "~> 0.11.0"
+
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Providers
+# ----------------------------------------------------------------------------------------------------------------------
+
+provider "aws" {
+  version = "~> 1.42.0"
+  region  = "${var.aws_region}"
+
+  assume_role {
+    role_arn = "arn:aws:iam::${var.account_id}:role/${var.account_assume_role_name}"
+  }
+}
+
+provider "aws" {
+  alias   = "master"
+  version = "~> 1.42.0"
+  region  = "${var.aws_region}"
+
+  assume_role {
+    role_arn = "arn:aws:iam::${var.master_account_id}:role/${var.master_account_assume_role_name}"
+  }
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Shared Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "parent_domain_name" {}
+
+variable "aws_subdomain_name" {
+  default = "aws"
+}
+
+locals {
+  dns_zone_name = "${join(".", compact(list(local.vpc_subdomain_name, var.aws_subdomain_name, var.parent_domain_name)))}"
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "name" {
+  default = "vpc"
+}
+
+variable "create" {
+  default = "true"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable terraform_module {
+  default = "gravicore/terraform-gravicore-modules/aws/vpc"
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Platform Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "namespace" {
+  default = "grv"
+}
+
+variable "environment" {}
+
+variable "stage" {
+  default = "dev"
+}
+
+variable "repository" {
+  default = ""
+}
+
+variable "master_account_id" {}
+variable "account_id" {}
+
+variable "master_account_assume_role_name" {
+  default = "grv_deploy_svc"
+}
+
+variable "account_assume_role_name" {
+  default = "OrganizationAccountAccessRole"
+}
+
+variable "desc_prefix" {
+  default = "Gravicore Module:"
+}
+
+variable "tags" {
+  default = {}
+}
+
+locals {
+  environment_prefix = "${join("-", list(var.namespace, var.environment))}"
+  stage_prefix       = "${join("-", list(var.namespace, var.environment, var.stage))}"
+  module_prefix      = "${join("-", list(var.namespace, var.environment, var.stage, var.name))}"
+
+  business_tags = {
+    Namespace   = "${var.namespace}"
+    Environment = "${var.environment}"
+  }
+
+  technical_tags = {
+    Stage           = "${var.stage}"
+    Repository      = "${var.repository}"
+    MasterAccountID = "${var.master_account_id}"
+    AccountID       = "${var.account_id}"
+    TerraformModule = "${var.terraform_module}"
+  }
+
+  automation_tags = {}
+
+  security_tags = {}
+
+  tags = "${merge(
+    local.business_tags,
+    local.technical_tags,
+    local.automation_tags,
+    local.security_tags,
+    var.tags
+  )}"
+}
---
+++ aws/spoke-vpc/test-instance.tf
@@ -0,0 +1,159 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# VARIABLES / LOCALS / REMOTE STATE
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "create_test_instance" {
+  default = "false"
+}
+
+variable "test_instance_type" {
+  default = "t2.nano"
+}
+
+variable "test_ingress_cidr_block" {
+  default = "10.0.0.0/8"
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# MODULES / RESOURCES
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Create a default key/pair for public and private instances
+
+locals {
+  module_test_ssh_key_pair_public_tags = "${merge(local.tags, map(
+    "TerraformModule", "cloudposse/terraform-aws-key-pair",
+    "TerraformModuleVersion", "0.2.5"))}"
+}
+
+module "ssh_key_pair_public" {
+  source    = "git::https://github.com/cloudposse/terraform-aws-key-pair.git?ref=0.2.5"
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "${var.environment}-${var.name}-public"
+  tags      = "${local.module_test_ssh_key_pair_public_tags}"
+
+  ssh_public_key_path   = "${pathexpand("~/.ssh")}/${var.namespace}"
+  generate_ssh_key      = "true"
+  private_key_extension = ".pem"
+  public_key_extension  = ".pub"
+  chmod_command         = "chmod 600 %v"
+}
+
+locals {
+  module_test_ssh_key_pair_private_tags = "${merge(local.tags, map(
+    "TerraformModule", "gravicore/terraform-gravicore-modules/aws/shared-vpc/key-pair",
+    "TerraformModuleVersion", "issues/4"))}"
+}
+
+module "ssh_key_pair_private" {
+  source    = "./key-pair"
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "${var.environment}-${var.name}-private"
+  tags      = "${local.module_test_ssh_key_pair_private_tags}"
+
+  ssh_public_key_path   = "${pathexpand("~/.ssh")}/${var.namespace}"
+  generate_ssh_key      = "true"
+  private_key_extension = ".pem"
+  public_key_extension  = ".pub"
+  chmod_command         = "chmod 600 %v"
+}
+
+locals {
+  module_test_ssh_ec2_instance_secret_tags = "${merge(local.tags, map(
+    "TerraformModule", "cloudposse/terraform-aws-ssm-parameter-store",
+    "TerraformModuleVersion", "0.2.5"))}"
+}
+
+module "test_ssh_ec2_instance_secret" {
+  source = "git::https://github.com/cloudposse/terraform-aws-ssm-parameter-store?ref=0.1.5"
+  tags   = "${local.module_test_ssh_ec2_instance_secret_tags}"
+
+  kms_arn = "alias/parameter_store_key"
+
+  parameter_write = [
+    {
+      name        = "/${local.stage_prefix}/${var.name}-test-pem"
+      value       = "${module.ssh_key_pair_private.private_key}"
+      type        = "SecureString"
+      overwrite   = "true"
+      description = "${join(" ", list(var.desc_prefix, "VPC Test SSH Instance Private Key"))}"
+    },
+    {
+      name        = "/${local.stage_prefix}/${var.name}-test-pub"
+      value       = "${module.ssh_key_pair_private.public_key}"
+      type        = "SecureString"
+      overwrite   = "true"
+      description = "${join(" ", list(var.desc_prefix, "VPC Test SSH Instance Public Key"))}"
+    },
+  ]
+}
+
+locals {
+  module_test_ssh_sg_tags = "${merge(local.tags, map(
+    "TerraformModule", "terraform-aws-modules/security-group/aws",
+    "TerraformModuleVersion", "2.9.0"))}"
+}
+
+module "test_ssh_sg" {
+  source      = "terraform-aws-modules/security-group/aws//modules/ssh"
+  version     = "2.9.0"
+  create      = "${var.create_test_instance == "true" ? true : false}"
+  name        = "${local.module_prefix}-test"
+  description = "${join(" ", list(var.desc_prefix, "Test SSH Instance"))}"
+  tags        = "${local.module_test_ssh_sg_tags}"
+
+  vpc_id              = "${module.vpc.vpc_id}"
+  ingress_cidr_blocks = ["${var.test_ingress_cidr_block}"]
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = -1
+      to_port     = -1
+      protocol    = "icmp"
+      description = "All ICMP - IPv4"
+      cidr_blocks = "${var.test_ingress_cidr_block}"
+    },
+  ]
+}
+
+# Test EC2 instance
+locals {
+  module_test_ssh_ec2_instance_tags = "${merge(local.tags, map(
+    "TerraformModule", "cloudposse/terraform-aws-ec2-instance",
+    "TerraformModuleVersion", "0.7.5"))}"
+}
+
+module "test_ssh_ec2_instance" {
+  source           = "git::https://github.com/cloudposse/terraform-aws-ec2-instance.git?ref=0.7.5"
+  instance_enabled = "${var.create_test_instance}"
+  namespace        = ""
+  stage            = ""
+  name             = "${local.module_prefix}-test"
+  tags             = "${local.module_test_ssh_ec2_instance_tags}"
+
+  ssh_key_pair                = "${module.ssh_key_pair_private.key_name}"
+  instance_type               = "${var.test_instance_type}"
+  vpc_id                      = "${module.vpc.vpc_id}"
+  security_groups             = ["${module.test_ssh_sg.this_security_group_id}"]
+  subnet                      = "${module.vpc.private_subnets[0]}"
+  assign_eip_address          = "false"
+  associate_public_ip_address = "false"
+}
+
+resource "aws_route53_record" "test_ssh_ec2_instance" {
+  count    = "${var.create_test_instance == "true" ? 1 : 0}"
+  provider = "aws.master"
+
+  zone_id = "${aws_route53_zone.vpc.zone_id}"
+  name    = "test.${local.dns_zone_name}"
+  type    = "A"
+  ttl     = "60"
+  records = ["${module.test_ssh_ec2_instance.private_ip}"]
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# OUTPUTS
+# ----------------------------------------------------------------------------------------------------------------------
+
