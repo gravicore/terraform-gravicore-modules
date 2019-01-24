69dc579c7d57f18e21b4de09d185c747c47ef4a3
Tyson Beck <tyson.beck@gravicore.io>
2019-01-24 14:02:57 -0700

    #5 variablized all instance scheduler options to allow them to be overwritten.

--- aws/account/instance-scheduler.tf
+++ aws/account/instance-scheduler.tf
@@ -4,25 +4,25 @@ module "instance_scheduler" {
   # source                = "git::https://github.com/gravicore/terraform-gravicore-modules.git//aws/account/modules/instance-scheduler?ref=0.5.0"
   create                = "${var.create_instance_scheduler}"
   is_master             = "${local.is_master}"
-  TagName               = "Schedule"
-  ScheduledServices     = "Both"
-  SchedulingActive      = "Yes"
-  Regions               = "us-east-1,us-east-2,us-west-1,us-west-2"
-  DefaultTimezone       = "UTC"
+  TagName               = "${var.TagName}"
+  ScheduledServices     = "${var.ScheduledServices}"
+  SchedulingActive      = "${var.SchedulingActive}"
+  Regions               = "${var.Regions}"
+  DefaultTimezone       = "${var.DefaultTimezone}"
   child_account         = "${var.accounts}"
-  ScheduleLambdaAccount = "Yes"
-  SchedulerFrequency    = "5"
-  MemorySize            = "128"
+  ScheduleLambdaAccount = "${var.ScheduleLambdaAccount}"
+  SchedulerFrequency    = "${var.SchedulerFrequency}"
+  MemorySize            = "${var.MemorySize}"
 
   # Options
-  UseCloudWatchMetrics = "Yes"
-  SendAnonymousData    = "No"
-  Trace                = "Yes"
+  UseCloudWatchMetrics = "${var.UseCloudWatchMetrics}"
+  SendAnonymousData    = "${var.SendAnonymousData}"
+  Trace                = "${var.Trace}"
 
   # Other parameters
-  LogRetentionDays = "30"
-  StartedTags      = "ScheduleStatus=Started,ScheduleTimestamp={year}-{month}-{day} {hour}:{minute} {timezone}"
-  StoppedTags      = "ScheduleStatus=Stopped,ScheduleTimestamp={year}-{month}-{day} {hour}:{minute} {timezone}"
+  LogRetentionDays = "${var.LogRetentionDays}"
+  StartedTags      = "${var.StartedTags}"
+  StoppedTags      = "${var.StoppedTags}"
 }
 
 module "instance_scheduler_agent" {
@@ -39,3 +39,72 @@ module "instance_scheduler_agent" {
 #   is_child  = "${var.master_account_id != var.account_id ? 1 : 0 }"
 # }
 
+variable "TagName" {
+  description = "Name for tag key value"
+  default     = "Schedule"
+}
+
+variable "ScheduledServices" {
+  description = "Schedule EC2, RDS or Both"
+  default     = "Both"
+}
+
+variable "SchedulingActive" {
+  description = "Set schedules to be active."
+  default     = "Yes"
+}
+
+variable "Regions" {
+  description = "Regions to manage"
+  default     = "us-east-1,us-east-2,us-west-1,us-west-2"
+}
+
+variable "DefaultTimezone" {
+  description = "Default timezone"
+  default     = "UTC"
+}
+
+variable "ScheduleLambdaAccount" {
+  description = ""
+  default     = "Yes"
+}
+
+variable "SchedulerFrequency" {
+  description = "Frequency to run the scheduler in minutes"
+  default     = "5"
+}
+
+variable "MemorySize" {
+  description = "Max memory size for lambda function"
+  default     = "128"
+}
+
+variable "UseCloudWatchMetrics" {
+  description = "Whether to post to cloudwatch"
+  default     = "Yes"
+}
+
+variable "SendAnonymousData" {
+  description = ""
+  default     = "No"
+}
+
+variable "Trace" {
+  description = ""
+  default     = "Yes"
+}
+
+variable "LogRetentionDays" {
+  description = "Time to retain cloudwatch logs in days"
+  default     = "30"
+}
+
+variable "StartedTags" {
+  description = "Tag value to set when scheduler starts an instance"
+  default     = "ScheduleStatus=Started,ScheduleTimestamp={year}-{month}-{day} {hour}:{minute} {timezone}"
+}
+
+variable "StoppedTags" {
+  description = "Tag value to set when scheduler stops an instance"
+  default     = "ScheduleStatus=Stopped,ScheduleTimestamp={year}-{month}-{day} {hour}:{minute} {timezone}"
+}
