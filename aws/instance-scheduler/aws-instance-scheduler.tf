604a9f07732b982a7eca6ba275fe550b41b499c8
Tyson Beck <tyson.beck@gravicore.io>
2019-01-31 13:29:22 -0700

    #5 updated variables to be snake_case from TitleCase. Added is_standalone_sheculer variable and logic to deploy master scheduler in child account.

--- aws/instance-scheduler/aws-instance-scheduler.tf
+++ aws/instance-scheduler/aws-instance-scheduler.tf
@@ -1,35 +1,35 @@
 resource "aws_cloudformation_stack" "aws_instance_scheduler" {
-  count        = "${local.creat_instance_scheduler}"
+  count        = "${local.create_instance_scheduler}"
   name         = "aws-instance-scheduler"
   capabilities = ["CAPABILITY_IAM"]
 
   parameters {
-    TagName               = "${var.TagName}"
-    ScheduledServices     = "${var.ScheduledServices}"
-    SchedulingActive      = "${var.SchedulingActive}"
-    Regions               = "${var.Regions}"
-    DefaultTimezone       = "${var.DefaultTimezone}"
+    TagName               = "${var.tag_name}"
+    ScheduledServices     = "${var.scheduled_services}"
+    SchedulingActive      = "${var.scheduling_active}"
+    Regions               = "${var.regions}"
+    DefaultTimezone       = "${var.default_timezone}"
     CrossAccountRoles     = "${local.aws_instance_scheduler_cross_account_roles}"
-    ScheduleLambdaAccount = "${var.ScheduleLambdaAccount}"
-    SchedulerFrequency    = "${var.SchedulerFrequency}"
-    MemorySize            = "${var.MemorySize}"
+    ScheduleLambdaAccount = "${var.schedule_lambda_account}"
+    SchedulerFrequency    = "${var.scheduler_frequency}"
+    MemorySize            = "${var.memory_size}"
 
     # Options
-    UseCloudWatchMetrics = "${var.UseCloudWatchMetrics}"
-    SendAnonymousData    = "${var.SendAnonymousData}"
-    Trace                = "${var.Trace}"
+    UseCloudWatchMetrics = "${var.use_cloud_watch_metrics}"
+    SendAnonymousData    = "${var.send_anonymous_data}"
+    Trace                = "${var.trace}"
 
     # Other parameters
-    LogRetentionDays = "${var.LogRetentionDays}"
-    StartedTags      = "${var.StartedTags}"
-    StoppedTags      = "${var.StoppedTags}"
+    LogRetentionDays = "${var.log_retention_days}"
+    StartedTags      = "${var.started_tags}"
+    StoppedTags      = "${var.stopped_tags}"
   }
 
   template_body = "${file("${path.module}/cloudformation/aws-instance-scheduler.cft")}"
 }
 
 resource "aws_cloudformation_stack" "schedule" {
-  count        = "${local.creat_instance_scheduler}"
+  count        = "${local.create_instance_scheduler}"
   depends_on   = ["aws_cloudformation_stack.aws_instance_scheduler"]
   name         = "schedule"
   capabilities = ["CAPABILITY_IAM"]
@@ -44,12 +44,12 @@ resource "aws_cloudformation_stack" "schedule" {
 module "instance_scheduler_agent" {
   source = "./instance-scheduler-agent"
 
-  create            = "${local.creat_instance_scheduler_agent}"
+  create            = "${local.create_instance_scheduler_agent}"
   master_account_id = "${var.master_account_id}"
 }
 
 locals {
-  creat_instance_scheduler                   = "${var.create == 1 ? local.is_master : 0 }"
-  creat_instance_scheduler_agent             = "${var.create == 1 ? local.is_child : 0 }"
-  aws_instance_scheduler_cross_account_roles = "${join(",",(formatlist("arn:aws:iam::%s:role/aws-instance-scheduler-re-EC2SchedulerCrossAccount", var.accounts)))}"
+  create_instance_scheduler                  = "${coalesce(var.is_standalone_scheduler, local.is_master)}"
+  create_instance_scheduler_agent            = "${local.create_instance_scheduler == 1 ? 0 : local.is_child }"
+  aws_instance_scheduler_cross_account_roles = "${var.is_standalone_scheduler == 1 ? "" : coalesce(var.cross_account_roles, join(",",(formatlist("arn:aws:iam::%s:role/aws-instance-scheduler-re-EC2SchedulerCrossAccount", var.accounts)))) }"
 }
--- aws/instance-scheduler/variables.tf
+++ aws/instance-scheduler/variables.tf
@@ -10,37 +10,37 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "SchedulingActive" {
+variable "scheduling_active" {
   type        = "string"
-  default     = "yes"
+  default     = "Yes"
   description = "Activate or deactivate scheduling."
 }
 
-variable "ScheduledServices" {
+variable "scheduled_services" {
   type        = "string"
   default     = "Both"
   description = "Scheduled Services. Allowed values: EC2, RDS, Both"
 }
 
-variable "MemorySize" {
+variable "memory_size" {
   default     = "128"
   description = "Size of the Lambda function running the scheduler, increase size when processing large numbers of instances. Allowed values: 128, 384, 512, 640, 768, 896, 1024, 1152, 1280, 1408, 1536"
 }
 
-variable "UseCloudWatchMetrics" {
+variable "use_cloud_watch_metrics" {
   type        = "string"
-  default     = "no"
+  default     = "Yes"
   description = "Collect instance scheduling data using CloudWatch metrics."
 }
 
-variable "LogRetentionDays" {
+variable "log_retention_days" {
   default     = "30"
   description = "Retention days for scheduler logs."
 }
 
-variable "Trace" {
+variable "trace" {
   type        = "string"
-  default     = "No"
+  default     = "Yes"
   description = "Enable logging of detailed informtion in CloudWatch logs."
 }
 
@@ -50,60 +50,63 @@ variable "child_account" {
   description = "Child accounts to create destination points for"
 }
 
-variable "TagName" {
+variable "tag_name" {
   default     = "Schedule"
   type        = "string"
   description = "Name of tag to use for associating instance schedule schemas with service instances."
 }
 
-variable "DefaultTimezone" {
+variable "default_timezone" {
   type        = "string"
   default     = "UTC"
   description = "Choose the default Time Zone. Default is 'UTC'"
 }
 
-variable "Regions" {
+variable "regions" {
   type        = "string"
-  default     = ""
+  default     = "us-east-1"
   description = "Comma separated list of regions in which instances are scheduled, leave blank for current region only."
 }
 
-variable "CrossAccountRoles" {
+variable "cross_account_roles" {
   type        = "string"
   default     = ""
   description = "Comma separated list of ARN's for cross account access roles. These roles must be created in all checked accounts the scheduler to start and stop instances."
 }
 
-variable "StartedTags" {
+variable "started_tags" {
   type        = "string"
+  default     = "ScheduleStatus=Started,ScheduleTimestamp={year}-{month}-{day} {hour}:{minute} {timezone}"
   description = "Comma separated list of tagname and values on the formt name=value,name=value,.. that are set on started instances"
 }
 
-variable "StoppedTags" {
+variable "stopped_tags" {
   type        = "string"
+  default     = "ScheduleStatus=Stopped,ScheduleTimestamp={year}-{month}-{day} {hour}:{minute} {timezone}"
   description = "Comma separated list of tagname and values on the formt name=value,name=value,.. that are set on stopped instances"
 }
 
-variable "SchedulerFrequency" {
+variable "scheduler_frequency" {
   type        = "string"
   default     = "5"
   description = "Scheduler running frequency in minutes. Allowed values: 1, 2, 5, 10, 15, 30, 60"
 }
 
-variable "ScheduleLambdaAccount" {
+variable "schedule_lambda_account" {
   type        = "string"
   default     = "Yes"
   description = "Schedule instances in this account."
 }
 
-variable "SendAnonymousData" {
+variable "send_anonymous_data" {
   type        = "string"
-  default     = "Yes"
+  default     = "No"
   description = "Send Anonymous Metrics Data."
 }
 
-variable "create" {
-  default = true
+variable "is_standalone_scheduler" {
+  default     = ""
+  description = "Will deploy the master scheduler instead of the agent if set to true."
 }
 
 variable "accounts" {
