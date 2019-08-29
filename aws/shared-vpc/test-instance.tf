6c614167ea3b73e6e197fceaa6dbb49aea3ecbc8
Tyson Beck <tyson.beck@gravicore.io>
2019-08-29 12:44:12 -0600

    #54 Enabled available resources to be toggled on or off via variables
    Updated VPC module to most current terraform 11 compatible release.
    Added variables for all available resources and endpoints.
    Moved SSH key generation out of test instance and into main module.

--- aws/shared-vpc/test-instance.tf
+++ aws/shared-vpc/test-instance.tf
@@ -14,91 +14,9 @@ variable "test_ingress_cidr_block" {
   default = "10.0.0.0/8"
 }
 
-variable "kms_arn" {
-  type        = "string"
-  default     = "alias/parameter_store_key"
-  description = "The ARN of a KMS key used to encrypt and decrypt SecretString values"
-}
-
 # ----------------------------------------------------------------------------------------------------------------------
 # MODULES / RESOURCES
 # ----------------------------------------------------------------------------------------------------------------------
-
-# Create a default key/pair for public and private instances
-
-locals {
-  module_test_ssh_key_pair_public_tags = "${merge(local.tags, map(
-    "TerraformModule", "cloudposse/terraform-aws-key-pair",
-    "TerraformModuleVersion", "0.2.5"))}"
-}
-
-module "ssh_key_pair_public" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-key-pair.git?ref=0.2.5"
-  namespace = "${var.namespace}"
-  stage     = "${var.stage}"
-  name      = "${var.environment}-${var.name}-public"
-  tags      = "${local.module_test_ssh_key_pair_public_tags}"
-
-  ssh_public_key_path   = "${pathexpand("~/.ssh")}/${var.namespace}"
-  generate_ssh_key      = "true"
-  private_key_extension = ".pem"
-  public_key_extension  = ".pub"
-  chmod_command         = "chmod 600 %v"
-}
-
-locals {
-  module_test_ssh_key_pair_private_tags = "${merge(local.tags, map(
-    "TerraformModule", "gravicore/terraform-gravicore-modules/aws/shared-vpc/key-pair",
-    "TerraformModuleVersion", "issues/4"))}"
-}
-
-module "ssh_key_pair_private" {
-  source    = "./key-pair"
-  namespace = "${var.namespace}"
-  stage     = "${var.stage}"
-  name      = "${var.environment}-${var.name}-private"
-  tags      = "${local.module_test_ssh_key_pair_private_tags}"
-
-  ssh_public_key_path   = "${pathexpand("~/.ssh")}/${var.namespace}"
-  generate_ssh_key      = "true"
-  private_key_extension = ".pem"
-  public_key_extension  = ".pub"
-  chmod_command         = "chmod 600 %v"
-}
-
-locals {
-  test_ssh_ec2_instance_secret_ssm_write = [
-    {
-      name        = "/${local.stage_prefix}/${var.name}-test-pem"
-      value       = "${module.ssh_key_pair_private.private_key}"
-      type        = "SecureString"
-      overwrite   = "true"
-      description = "${join(" ", list(var.desc_prefix, "VPC Test SSH Instance Private Key"))}"
-    },
-    {
-      name        = "/${local.stage_prefix}/${var.name}-test-pub"
-      value       = "${module.ssh_key_pair_private.public_key}"
-      type        = "SecureString"
-      overwrite   = "true"
-      description = "${join(" ", list(var.desc_prefix, "VPC Test SSH Instance Public Key"))}"
-    },
-  ]
-
-  # `test_ssh_ec2_instance_secret_ssm_write_count` needs to be updated if `test_ssh_ec2_instance_secret_ssm_write` changes
-  test_ssh_ec2_instance_secret_ssm_write_count = 2
-}
-
-resource "aws_ssm_parameter" "default" {
-  count           = "${var.create_test_instance == "true" ? local.test_ssh_ec2_instance_secret_ssm_write_count : 0}"
-  name            = "${lookup(local.test_ssh_ec2_instance_secret_ssm_write[count.index], "name")}"
-  description     = "${lookup(local.test_ssh_ec2_instance_secret_ssm_write[count.index], "description", lookup(local.test_ssh_ec2_instance_secret_ssm_write[count.index], "name"))}"
-  type            = "${lookup(local.test_ssh_ec2_instance_secret_ssm_write[count.index], "type", "SecureString")}"
-  key_id          = "${lookup(local.test_ssh_ec2_instance_secret_ssm_write[count.index], "type", "SecureString") == "SecureString" && length(var.kms_arn) > 0 ? var.kms_arn : ""}"
-  value           = "${lookup(local.test_ssh_ec2_instance_secret_ssm_write[count.index], "value")}"
-  overwrite       = "${lookup(local.test_ssh_ec2_instance_secret_ssm_write[count.index], "overwrite", "false")}"
-  allowed_pattern = "${lookup(local.test_ssh_ec2_instance_secret_ssm_write[count.index], "allowed_pattern", "")}"
-  tags            = "${local.tags}"
-}
 
 locals {
   module_test_ssh_sg_tags = "${merge(local.tags, map(
