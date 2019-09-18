1caa81ad432d91d98a773da69a0a0f4d7e1f1c24
Tyson Beck <tyson.beck@gravicore.io>
2019-09-18 09:20:56 -0600

    #57 Updated Aviatrix Controller to TF12

--- aws/aviatrix/modules/controller/modules/aviatrix-controller-build/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "private_ip" {
-  value = "${aws_instance.aviatrixcontroller.*.private_ip[0]}"
-}
-
-output "public_ip" {
-  value = "${aws_eip.controller_eip.*.public_ip[0]}"
-}
--- aws/aviatrix/modules/controller/resources.tf
+++ aws/aviatrix/modules/controller/resources.tf
@@ -1,9 +1,9 @@
 module "ssh_key_pair" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-key-pair.git?ref=0.2.5"
-  namespace = "${var.namespace}"
-  stage     = "${var.stage}"
+  source    = "git::https://github.com/cloudposse/terraform-aws-key-pair.git?ref=0.4.0"
+  namespace = var.namespace
+  stage     = var.stage
   name      = "${var.environment}-${var.name}"
-  tags      = "${local.tags}"
+  tags      = local.tags
 
   ssh_public_key_path   = "${pathexpand("~/.ssh")}/${var.namespace}"
   generate_ssh_key      = "true"
@@ -14,14 +14,15 @@ module "ssh_key_pair" {
 
 module "aviatrix_controller" {
   # source = "github.com/AviatrixSystems/terraform-modules.git/aviatrix-controller-build"
-  source = "./modules/aviatrix-controller-build"
+  source = "./aviatrix-controller-build"
 
-  vpc = "${data.terraform_remote_state.vpc.vpc_id}"
+  vpc = data.terraform_remote_state.vpc.outputs.vpc_id
 
-  subnet = "${data.terraform_remote_state.vpc.vpc_public_subnets[0]}"
+  subnet = data.terraform_remote_state.vpc.outputs.vpc_public_subnets[0]
 
   # subnet  = "${data.terraform_remote_state.vpc.public_subnets[0]}"
-  keypair = "${module.ssh_key_pair.key_name}"
+  keypair = module.ssh_key_pair.key_name
   ec2role = "aviatrix-role-ec2"
-  tags    = "${local.tags}"
+  tags    = local.tags
 }
+
