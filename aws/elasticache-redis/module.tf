8852d086daae689d29dfaa169873a248d59d82b8
Tyson Beck <tyson.beck@gravicore.io>
2019-09-19 16:06:55 -0600

    #57 Updated Elasticache Redis module to TF12

--- aws/elasticache-redis/module.tf
+++ aws/elasticache-redis/module.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 0.11.2"
+  required_version = ">= 0.12"
 
   # The configuration for this backend will be filled in by Terragrunt
   backend "s3" {}
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   version = "~> 2.26.0"
-  region  = "${var.aws_region}"
+  region  = var.aws_region
 
   assume_role {
     role_arn = "arn:aws:iam::${var.account_id}:role/${var.account_assume_role_name}"
@@ -21,7 +21,7 @@ provider "aws" {
 provider "aws" {
   alias   = "master"
   version = "~> 2.26.0"
-  region  = "${var.aws_region}"
+  region  = var.aws_region
 
   assume_role {
     role_arn = "arn:aws:iam::${var.master_account_id}:role/${var.master_account_assume_role_name}"
@@ -41,14 +41,14 @@ variable "name" {
 }
 
 variable "create" {
-  default = "true"
+  default = true
 }
 
 variable "aws_region" {
   default = "us-east-1"
 }
 
-variable terraform_module {
+variable "terraform_module" {
   default = "gravicore/terraform-gravicore-modules/aws/elasticache-redis"
 }
 
@@ -60,7 +60,8 @@ variable "namespace" {
   default = "grv"
 }
 
-variable "environment" {}
+variable "environment" {
+}
 
 variable "stage" {
   default = "dev"
@@ -70,8 +71,11 @@ variable "repository" {
   default = ""
 }
 
-variable "master_account_id" {}
-variable "account_id" {}
+variable "master_account_id" {
+}
+
+variable "account_id" {
+}
 
 variable "master_account_assume_role_name" {
   default = "grv_deploy_svc"
@@ -90,32 +94,33 @@ variable "tags" {
 }
 
 locals {
-  environment_prefix = "${join("-", list(var.namespace, var.environment))}"
-  stage_prefix       = "${join("-", list(var.namespace, var.environment, var.stage))}"
-  module_prefix      = "${join("-", list(var.namespace, var.environment, var.stage, var.name))}"
+  environment_prefix = join("-", [var.namespace, var.environment])
+  stage_prefix       = join("-", [var.namespace, var.environment, var.stage])
+  module_prefix      = join("-", [var.namespace, var.environment, var.stage, var.name])
 
   business_tags = {
-    Namespace   = "${var.namespace}"
-    Environment = "${var.environment}"
+    Namespace   = var.namespace
+    Environment = var.environment
   }
 
   technical_tags = {
-    Stage           = "${var.stage}"
-    Repository      = "${var.repository}"
-    MasterAccountID = "${var.master_account_id}"
-    AccountID       = "${var.account_id}"
-    TerraformModule = "${var.terraform_module}"
+    Stage           = var.stage
+    Repository      = var.repository
+    MasterAccountID = var.master_account_id
+    AccountID       = var.account_id
+    TerraformModule = var.terraform_module
   }
 
   automation_tags = {}
 
   security_tags = {}
 
-  tags = "${merge(
+  tags = merge(
     local.business_tags,
     local.technical_tags,
     local.automation_tags,
     local.security_tags,
-    var.tags
-  )}"
+    var.tags,
+  )
 }
+
