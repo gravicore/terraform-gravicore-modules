bb701b8cd3c6a7d36e5b945582963f70c1589b53
Tyson Beck <tyson.beck@gravicore.io>
2019-09-19 16:06:13 -0600

    #57 Updated Spoke VPC module to TF12

--- aws/spoke-vpc/key-pair/submodule.tf
+++ aws/spoke-vpc/key-pair/submodule.tf
@@ -1,11 +1,11 @@
 module "label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  name       = "${var.name}"
-  attributes = "${var.attributes}"
-  delimiter  = "${var.delimiter}"
-  tags       = "${var.tags}"
+  namespace  = var.namespace
+  stage      = var.stage
+  name       = var.name
+  attributes = var.attributes
+  delimiter  = var.delimiter
+  tags       = var.tags
 }
 
 locals {
@@ -14,42 +14,43 @@ locals {
 }
 
 resource "aws_key_pair" "imported" {
-  count      = "${var.generate_ssh_key == "false" ? 1 : 0}"
-  key_name   = "${module.label.id}"
-  public_key = "${file("${local.public_key_filename}")}"
+  count      = var.generate_ssh_key ? 1 : 0
+  key_name   = module.label.id
+  public_key = file(local.public_key_filename)
 }
 
 resource "tls_private_key" "default" {
-  count     = "${var.generate_ssh_key == "true" ? 1 : 0}"
-  algorithm = "${var.ssh_key_algorithm}"
+  count     = var.generate_ssh_key ? 1 : 0
+  algorithm = var.ssh_key_algorithm
 }
 
 resource "aws_key_pair" "generated" {
-  count      = "${var.generate_ssh_key == "true" ? 1 : 0}"
-  depends_on = ["tls_private_key.default"]
-  key_name   = "${module.label.id}"
-  public_key = "${tls_private_key.default.public_key_openssh}"
+  count      = var.generate_ssh_key ? 1 : 0
+  depends_on = [tls_private_key.default]
+  key_name   = module.label.id
+  public_key = tls_private_key.default[0].public_key_openssh
 }
 
 resource "local_file" "public_key_openssh" {
-  count      = "${var.generate_ssh_key == "true" ? 1 : 0}"
-  depends_on = ["tls_private_key.default"]
-  content    = "${tls_private_key.default.public_key_openssh}"
-  filename   = "${local.public_key_filename}"
+  count      = var.generate_ssh_key ? 1 : 0
+  depends_on = [tls_private_key.default]
+  content    = tls_private_key.default[0].public_key_openssh
+  filename   = local.public_key_filename
 }
 
 resource "local_file" "private_key_pem" {
-  count      = "${var.generate_ssh_key == "true" ? 1 : 0}"
-  depends_on = ["tls_private_key.default"]
-  content    = "${tls_private_key.default.private_key_pem}"
-  filename   = "${local.private_key_filename}"
+  count      = var.generate_ssh_key ? 1 : 0
+  depends_on = [tls_private_key.default]
+  content    = tls_private_key.default[0].private_key_pem
+  filename   = local.private_key_filename
 }
 
 resource "null_resource" "chmod" {
-  count      = "${var.generate_ssh_key == "true" && var.chmod_command != "" ? 1 : 0}"
-  depends_on = ["local_file.private_key_pem"]
+  count      = var.generate_ssh_key && var.chmod_command != "" ? 1 : 0
+  depends_on = [local_file.private_key_pem]
 
   provisioner "local-exec" {
-    command = "${format(var.chmod_command, local.private_key_filename)}"
+    command = format(var.chmod_command, local.private_key_filename)
   }
 }
+
