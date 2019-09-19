e46667786d339c060a0adb1d85d53a0c404d1c45
Tyson Beck <tyson.beck@gravicore.io>
2019-09-19 16:07:49 -0600

    #57 Updated central logging module to TF12

--- aws/central-logging/agent/central-logging-agent.tf
+++ aws/central-logging/agent/central-logging-agent.tf
@@ -8,27 +8,28 @@ variable "terraform_remote_state_key" {
 
 variable "namespace" {
   description = "Namespace (e.g. `grv` or `gravicore`)"
-  type        = "string"
+  type        = string
 }
 
 variable "stage" {
   description = "Stage (e.g. `prod`, `uat`, `dev`)"
-  type        = "string"
+  type        = string
 }
 
 variable "environment" {
   description = "Environment (e.g. `master`)"
-  type        = "string"
+  type        = string
 }
 
 variable "repository" {
-  type = "string"
+  type = string
 }
 
-variable "master_account_id" {}
+variable "master_account_id" {
+}
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
 }
@@ -48,7 +49,7 @@ variable "account_id" {
   default     = ""
 }
 
-variable terraform_module {
+variable "terraform_module" {
   default = "gravicore/terraform-gravicore-modules/aws/central-logging/agent"
 }
 
@@ -57,7 +58,7 @@ variable terraform_module {
 # ----------------------------------------------------------------------------------------------------------------------
 
 data "aws_iam_policy_document" "log_assume" {
-  count = "${var.enabled == "true" ? 1 : 0}"
+  count = var.enabled == "true" ? 1 : 0
 
   statement {
     actions = ["sts:AssumeRole"]
@@ -86,30 +87,30 @@ data "aws_iam_policy_document" "log" {
 }
 
 resource "aws_iam_role_policy" "log" {
-  count  = "${var.enabled == "true" ? 1 : 0}"
+  count  = var.enabled == "true" ? 1 : 0
   name   = "${local.stage_prefix}-vpc-flow-logs"
-  role   = "${aws_iam_role.log.id}"
-  policy = "${data.aws_iam_policy_document.log.json}"
+  role   = aws_iam_role.log[0].id
+  policy = data.aws_iam_policy_document.log.json
 }
 
 resource "aws_iam_role" "log" {
-  count              = "${var.enabled == "true" ? 1 : 0}"
+  count              = var.enabled == "true" ? 1 : 0
   name               = "${local.stage_prefix}-vpc-flow-logs"
-  assume_role_policy = "${data.aws_iam_policy_document.log_assume.json}"
+  assume_role_policy = data.aws_iam_policy_document.log_assume[0].json
 }
 
 module "flow_log_destination" {
   source = "./central-logging-agent-destination"
 
-  master_account_assume_role_name = "${var.master_account_assume_role_name}"
-  terraform_remote_state_key      = "${var.terraform_remote_state_key}"
-  namespace                       = "${var.namespace}"
-  environment                     = "${var.environment}"
-  stage                           = "${var.stage}"
-  enabled                         = "${var.enabled}"
-  master_account_id               = "${var.master_account_id}"
-  account_id                      = "${var.account_id}"
-  repository                      = "${var.repository}"
+  master_account_assume_role_name = var.master_account_assume_role_name
+  terraform_remote_state_key      = var.terraform_remote_state_key
+  namespace                       = var.namespace
+  environment                     = var.environment
+  stage                           = var.stage
+  enabled                         = var.enabled
+  master_account_id               = var.master_account_id
+  account_id                      = var.account_id
+  repository                      = var.repository
   log_type                        = "flow-logs"
   filter_pattern                  = "[version, account, eni, source, destination, srcport, destport, protocol, packets, bytes, windowstart, windowend, action, flowlogstatus]"
 }
@@ -119,31 +120,32 @@ module "flow_log_destination" {
 # ----------------------------------------------------------------------------------------------------------------------
 
 output "flow_log_group_name" {
-  value       = "${module.flow_log_destination.log_group_name}"
+  value       = module.flow_log_destination.log_group_name
   description = "Name of the flow log log group"
 }
 
 output "log_group_iam_role_arn" {
-  value       = "${element(concat(aws_iam_role.log.*.arn, list("")), 0)}"
+  value       = element(concat(aws_iam_role.log.*.arn, [""]), 0)
   description = "The Amazon Resource Name (ARN) specifying the role"
 }
 
 output "log_group_iam_role_unique_id" {
-  value       = "${element(concat(aws_iam_role.log.*.unique_id, list("")), 0)}"
+  value       = element(concat(aws_iam_role.log.*.unique_id, [""]), 0)
   description = "The stable and unique string identifying the role"
 }
 
 output "log_group_iam_role_name" {
-  value       = "${element(concat(aws_iam_role.log.*.name, list("")), 0)}"
+  value       = element(concat(aws_iam_role.log.*.name, [""]), 0)
   description = "The name of the role."
 }
 
 output "flow_log_group_arn" {
-  value       = "${module.flow_log_destination.log_group_arn}"
+  value       = module.flow_log_destination.log_group_arn
   description = "The flow log log group's Amazon Resource Name (ARN) specifying the log group"
 }
 
 output "flow_log_destination_arn" {
-  value       = "${module.flow_log_destination.destination_arn}"
+  value       = module.flow_log_destination.destination_arn
   description = "The kinesis destination's Amazon Resource Name (ARN) specifying the log group"
 }
+
--- aws/central-logging/module.tf
+++ aws/central-logging/module.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 0.11.8"
+  required_version = ">= 0.12"
 
   # The configuration for this backend will be filled in by Terragrunt
   backend "s3" {}
@@ -7,7 +7,7 @@ terraform {
 
 provider "aws" {
   version = "~> 2.26.0"
-  region  = "${var.aws_region}"
+  region  = var.aws_region
 
   assume_role {
     role_arn = "arn:aws:iam::${var.account_id}:role/${var.account_assume_role_name}"
@@ -17,7 +17,7 @@ provider "aws" {
 provider "aws" {
   alias   = "master"
   version = "~> 2.26.0"
-  region  = "${var.aws_region}"
+  region  = var.aws_region
 
   assume_role {
     role_arn = "arn:aws:iam::${var.master_account_id}:role/${var.master_account_assume_role_name}"
@@ -25,37 +25,37 @@ provider "aws" {
 }
 
 locals {
-  is_master = "${var.master_account_id == var.account_id ? 1 : 0 }"
-  is_child  = "${var.master_account_id != var.account_id ? 1 : 0 }"
+  is_master = var.master_account_id == var.account_id ? 1 : 0
+  is_child  = var.master_account_id != var.account_id ? 1 : 0
 
-  environment_prefix = "${join("-", list(var.namespace, var.environment))}"
-  stage_prefix       = "${join("-", list(var.namespace, var.environment, var.stage))}"
-  module_prefix      = "${join("-", list(var.namespace, var.environment, var.stage, var.name))}"
+  environment_prefix = join("-", [var.namespace, var.environment])
+  stage_prefix       = join("-", [var.namespace, var.environment, var.stage])
+  module_prefix      = join("-", [var.namespace, var.environment, var.stage, var.name])
 
   business_tags = {
-    Namespace   = "${var.namespace}"
-    Environment = "${var.environment}"
+    Namespace   = var.namespace
+    Environment = var.environment
   }
 
   technical_tags = {
-    Stage           = "${var.stage}"
-    Repository      = "${var.repository}"
-    MasterAccountID = "${var.master_account_id}"
-    AccountID       = "${var.account_id}"
-    TerraformModule = "${var.terraform_module}"
+    Stage           = var.stage
+    Repository      = var.repository
+    MasterAccountID = var.master_account_id
+    AccountID       = var.account_id
+    TerraformModule = var.terraform_module
   }
 
   automation_tags = {}
 
   security_tags = {}
 
-  tags = "${merge(
+  tags = merge(
     local.technical_tags,
     local.business_tags,
     local.automation_tags,
     local.security_tags,
-    var.tags
-  )}"
+    var.tags,
+  )
 }
 
 variable "master_account_assume_role_name" {
@@ -65,3 +65,4 @@ variable "master_account_assume_role_name" {
 variable "account_assume_role_name" {
   default = "OrganizationAccountAccessRole"
 }
+
