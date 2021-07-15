49624a91f15cbee58707ec8ddb12288e0eb9038d
Tyson Beck <tyson.beck@gravicore.io>
2021-07-14 22:29:25 -0600

    GDEV-203 Adding datasync agent deployment

--- aws/data-transfer/agent/datasync_agents.tf
+++ aws/data-transfer/agent/datasync_agents.tf
@@ -17,6 +17,12 @@ variable "vpc_private_subnets" {
   type        = list(string)
 }
 
+variable vpc_endpoint_id {
+  type        = string
+  default     = ""
+  description = "(Optional) The ID of the VPC (virtual private cloud) endpoint that the agent has access to"
+}
+
 variable "datasync_ec2_instance_type" {
   description = "DataSync agent instance size must be at least 2xlarge"
   type        = string
@@ -83,6 +89,26 @@ module "datasync_ec2" {
   vpc_security_group_ids = [aws_security_group.datasync[0].id]
 }
 
+resource "aws_datasync_agent" "default" {
+  count = var.create ? 1 : 0
+  name  = join(var.delimiter, [local.module_prefix, "datasync"])
+  tags  = local.tags
+
+  ip_address            = module.datasync_ec2.private_ip[0]
+  security_group_arns   = ["arn:aws:ec2:${var.aws_region}:${var.account_id}:security-group/${aws_security_group.datasync[0].id}"]
+  subnet_arns           = [for s in var.vpc_private_subnets : "arn:aws:ec2:${var.aws_region}:${var.account_id}:subnet/${s}"]
+  vpc_endpoint_id       = var.vpc_endpoint_id
+  private_link_endpoint = data.aws_network_interface.datasync.private_ip
+}
+
+data "aws_vpc_endpoint" "datasync" {
+  id = var.vpc_endpoint_id
+}
+
+data "aws_network_interface" "datasync" {
+  id = tolist(data.aws_vpc_endpoint.datasync.network_interface_ids)[1]
+}
+
 # ----------------------------------------------------------------------------------------------------------------------
 # Outputs
 # ----------------------------------------------------------------------------------------------------------------------
@@ -105,4 +131,14 @@ output "datasync_agent_name" {
 output "datasync_agent_tags" {
   description = "Tags of the DataSync Agent"
   value       = var.create ? local.tags : null
+}
+
+output "datasync_agent_id" {
+  description = ""
+  value       = aws_datasync_agent.default.*.id
+}
+
+output "datasync_agent_arn" {
+  description = ""
+  value       = aws_datasync_agent.default.*.arn
 }
