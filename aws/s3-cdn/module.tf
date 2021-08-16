efc70e150d242144318f07b20cee70e84eff1f79
Benjamin Leon <benjamin.leon@gravicore.io>
2021-08-16 10:09:09 -0700

    GDEV-205 Update Module min Versions

--- aws/s3-cdn/module.tf
+++ aws/s3-cdn/module.tf
@@ -4,9 +4,9 @@ terraform {
 
   required_providers {
     aws      = ">= 2.0"
-    template = "~> 2.0"
-    local    = "~> 1.2"
-    null     = "~> 2.0"
+    template = ">= 2.0"
+    local    = ">= 1.2"
+    null     = ">= 2.0"
   }
 }
 
