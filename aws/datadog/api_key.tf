8e0f6349ae7d927c25cd132dea5e69a2c47bf53f
Jeremy Odle <jeremy.odle@gravicore.io>
2021-10-14 08:44:23 -0600

    GDEV-214 Set api key recovery window to 0 by default

--- aws/datadog/api_key.tf
+++ aws/datadog/api_key.tf
@@ -12,7 +12,7 @@ variable "datadog_app_key" {
 
 variable datadog_api_key_recovery_window_in_days {
   description = "Number of days that AWS Secrets Manager waits before it can delete the secret. This value can be 0 to force deletion without recovery or range from 7 to 30 days. The default value is 30."
-  default     = null
+  default     = 0
 }
 
 # ----------------------------------------------------------------------------------------------------------------------
