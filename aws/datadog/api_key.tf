3252ab3c30be04d53e58b49cdea2990ccd5151c8
Jeremy Odle <jeremy.odle@gravicore.io>
2021-10-14 08:11:08 -0600

    GDEV-214 Added Datadog module using vendor CloudFormation

--- aws/datadog/api_key.tf
+++ aws/datadog/api_key.tf
@@ -10,18 +10,28 @@ variable "datadog_app_key" {
   description = "Datadog APP key. This can also be set via the DATADOG_APP_KEY environment variable."
 }
 
+variable datadog_api_key_recovery_window_in_days {
+  description = "Number of days that AWS Secrets Manager waits before it can delete the secret. This value can be 0 to force deletion without recovery or range from 7 to 30 days. The default value is 30."
+  default     = null
+}
+
 # ----------------------------------------------------------------------------------------------------------------------
 # MODULES / RESOURCES
 # ----------------------------------------------------------------------------------------------------------------------
 
 resource "aws_secretsmanager_secret" "datadog_api_key" {
+  count       = var.create ? 1 : 0
   name        = "${local.module_prefix}-api-key"
   description = "Encrypted Datadog API Key"
   tags        = local.tags
+
+  recovery_window_in_days = var.datadog_api_key_recovery_window_in_days
 }
 
 resource "aws_secretsmanager_secret_version" "datadog_api_key" {
-  secret_id     = aws_secretsmanager_secret.datadog_api_key.id
+  count = var.create ? 1 : 0
+
+  secret_id     = concat(aws_secretsmanager_secret.datadog_api_key.*.id, [""])[0]
   secret_string = var.datadog_api_key
 }
 
@@ -30,5 +40,5 @@ resource "aws_secretsmanager_secret_version" "datadog_api_key" {
 # ----------------------------------------------------------------------------------------------------------------------
 
 output "datadog_api_key_arn" {
-  value = aws_secretsmanager_secret.datadog_api_key.arn
+  value = concat(aws_secretsmanager_secret.datadog_api_key.*.arn, [""])[0]
 }
--- aws/datadog/integration.tf
+++ aws/datadog/integration.tf
@@ -2,6 +2,18 @@
 # VARIABLES / LOCALS / REMOTE STATE
 # ----------------------------------------------------------------------------------------------------------------------
 
+variable "datadog_aws_account_specific_namespace_rules" {
+  description = "Enables or disables metric collection for specific AWS namespaces for this AWS account only. A list of namespaces can be found at the available namespace rules API endpoint."
+  type        = map
+  default     = {}
+}
+
+variable datadog_excluded_regions {
+  description = "An array of AWS regions to exclude from metrics collection."
+  type        = list(string)
+  default     = null
+}
+
 variable "datadog_integration_role_name" {
   description = "Then name of the AWS role for Datadog to assume"
   type        = string
@@ -20,10 +32,16 @@ variable "datadog_aws_host_tags" {
   default     = {}
 }
 
-variable "datadog_aws_account_specific_namespace_rules" {
-  description = "Enables or disables metric collection for specific AWS namespaces for this AWS account only. A list of namespaces can be found at the available namespace rules API endpoint."
-  type        = map
-  default     = {}
+variable "datadog_base_permissions" {
+  type        = string
+  default     = null
+  description = "Customize the base permissions for the Datadog IAM role. Select 'Core' to only grant Datadog permissions to a very limited set of metrics and metadata (not recommended)."
+}
+
+variable "datadog_site" {
+  type        = string
+  default     = null
+  description = "Define your Datadog Site to send data to. For the Datadog EU site, set to datadoghq.eu"
 }
 
 variable "datadog_log_archives" {
@@ -38,12 +56,11 @@ variable "datadog_cloudtrails" {
   description = "S3 buckets for Datadog CloudTrail integration. Separate multiple buckets with comma, e.g., 'bucket1,bucket2'. Permissions will be automatically added to the Datadog integration IAM role. https://docs.datadoghq.com/integrations/amazon_cloudtrail/"
 }
 
-variable "datadog_aws_account_id" {
+variable "datadog_cloud_security_posture_management_permissions" {
   type        = string
-  default     = "464622532012"
-  description = "Datadog AWS account ID allowed to assume the integration IAM role. DO NOT CHANGE!"
+  default     = null
+  description = "Set this value to 'true' to add permissions for Datadog to monitor your AWS cloud resource configurations. You need this set to 'true' to use Cloud Security Posture Management. You will also need 'BasePermissions' set to 'Full'."
 }
-
 
 locals {
   datadog_integration_role_name = join("-", [local.module_prefix, "integration"])
@@ -69,10 +86,11 @@ resource "datadog_integration_aws" "datadog_integration" {
   count = var.create ? 1 : 0
 
   account_id                       = local.account_id
-  role_name                        = local.datadog_integration_role_name
+  account_specific_namespace_rules = var.datadog_aws_account_specific_namespace_rules
+  excluded_regions                 = var.datadog_excluded_regions
   filter_tags                      = local.datadog_aws_filter_tags
   host_tags                        = local.datadog_aws_host_tags
-  account_specific_namespace_rules = var.datadog_aws_account_specific_namespace_rules
+  role_name                        = local.datadog_integration_role_name
 }
 
 # A Macro used to generate policies for the integration IAM role based on user inputs
@@ -86,6 +104,53 @@ resource "aws_cloudformation_stack" "datadog_policy_macro" {
 }
 
 # The IAM role for Datadog integration
+# Parameters:
+#   ExternalId:
+#     Description: >-
+#       External ID for the Datadog role (generate at
+#       https://app.datadoghq.com/account/settings#integrations/amazon-web-services)
+#     Type: String
+#   IAMRoleName:
+#     Description: Customize the name of IAM role for Datadog AWS integration
+#     Type: String  
+#     Default: DatadogIntegrationRole
+#   BasePermissions:
+#     Description: >-
+#       Customize the base permissions for the Datadog IAM role.
+#       Select "Core" to only grant Datadog permissions to a very limited set of metrics and metadata (not recommended).
+#     Type: String
+#     Default: Full
+#     AllowedValues:
+#       - Full
+#       - Core
+#   LogArchives:
+#     Description: >-
+#       S3 paths to store log archives for log rehydration. Separate multiple paths with comma,
+#       e.g., "my-bucket,my-bucket-with-path/path".
+#     Type: String
+#     Default: ''
+#   CloudTrails:
+#     Description: >-
+#       S3 buckets for the Datadog CloudTrail integration. Separate multiple buckets with commas,
+#       e.g., "bucket1,bucket2". Permissions will be automatically added to the Datadog integration IAM role.
+#       https://docs.datadoghq.com/integrations/amazon_cloudtrail/
+#     Type: String
+#     Default: ''
+#   CloudSecurityPostureManagementPermissions:
+#     Type: String
+#     Default: false
+#     AllowedValues:
+#       - true
+#       - false
+#     Description: >-
+#       Set this value to "true" to add permissions for Datadog's Cloud Security Posture Management product
+#       to monitor your AWS cloud resource configurations.
+#       You need this set to "true" to use Cloud Security Posture Management. You will also need "BasePermissions" set to "Full".
+#   DdAWSAccountId:
+#     Description: >-
+#       Datadog AWS account ID allowed to assume the integration IAM role. DO NOT CHANGE!
+#     Type: String
+#     Default: "464622532012"
 resource "aws_cloudformation_stack" "datadog_integration" {
   count = var.create ? 1 : 0
   name  = local.datadog_integration_role_name
@@ -94,12 +159,16 @@ resource "aws_cloudformation_stack" "datadog_integration" {
   template_url = "https://datadog-cloudformation-template.s3.amazonaws.com/aws/datadog_integration_role.yaml"
   capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
   parameters = {
-    ExternalId     = concat(datadog_integration_aws.datadog_integration.*.external_id, [""])[0]
-    Permissions    = "Full"
-    IAMRoleName    = local.datadog_integration_role_name
-    LogArchives    = var.datadog_log_archives
-    CloudTrails    = var.datadog_cloudtrails
-    DdAWSAccountId = var.datadog_aws_account_id
+    # Required
+    ExternalId      = concat(datadog_integration_aws.datadog_integration.*.external_id, [""])[0]
+    BasePermissions = var.datadog_base_permissions
+    IAMRoleName     = local.datadog_integration_role_name
+    # Optional
+    LogArchives                               = var.datadog_log_archives
+    CloudTrails                               = var.datadog_cloudtrails
+    CloudSecurityPostureManagementPermissions = var.datadog_cloud_security_posture_management_permissions
+    # Advanced
+    # DdAWSAccountId = var.datadog_aws_account_id
   }
 
   depends_on = [aws_cloudformation_stack.datadog_policy_macro]
@@ -109,11 +178,31 @@ resource "aws_cloudformation_stack" "datadog_integration" {
 # OUTPUTS
 # ----------------------------------------------------------------------------------------------------------------------
 
-output "account_id" {
+output account_id {
   value = var.account_id
 }
 
-output "datadog_external_id" {
+output datadog_id {
+  value = concat(datadog_integration_aws.datadog_integration.*.id, [""])[0]
+}
+
+output datadog_account_specific_namespace_rules {
+  value = concat(datadog_integration_aws.datadog_integration.*.account_specific_namespace_rules, [""])[0]
+}
+
+output datadog_excluded_regions {
+  value = concat(datadog_integration_aws.datadog_integration.*.excluded_regions, [""])[0]
+}
+
+output datadog_external_id {
   sensitive = true
   value     = concat(datadog_integration_aws.datadog_integration.*.external_id, [""])[0]
+}
+
+output datadog_filter_tags {
+  value = concat(datadog_integration_aws.datadog_integration.*.filter_tags, [""])[0]
+}
+
+output datadog_role_name {
+  value = concat(datadog_integration_aws.datadog_integration.*.role_name, [""])[0]
 }
