a2fa2ce18a9b1f3cb01f4371918d42d5db78143d
Benjamin Leon <benjamin.leon@gravicore.io>
2022-06-23 11:41:42 -0700

    GDEV-242 Version change

--- aws/ecs/module.tf
+++ aws/ecs/module.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = "~> 0.12.0"
 
   required_providers {
-    aws      = "~> 2.0"
+    aws      = "~> 3.74"
     template = "~> 2.0"
     local    = "~> 1.2"
     null     = "~> 2.0"
