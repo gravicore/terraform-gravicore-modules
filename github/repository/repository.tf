9fc7c6cd8bcb3ac15063c9274f76bd6eddcaa591
Benjamin Leon <benjamin.leon@gravicore.io>
2022-08-15 10:56:53 -0700

    GDEV-245 Default update

--- github/repository/repository.tf
+++ github/repository/repository.tf
@@ -130,7 +130,7 @@ variable "topics" {
 
 variable "vulnerability_alerts" {
   type        = bool
-  default     = false
+  default     = true
   description = "(Optional) - Set to true to enable security alerts for vulnerable dependencies. Enabling requires alerts to be enabled on the owner level. (Note for importing: GitHub enables the alerts on public repos but disables them on private repos by default.) See https://help.github.com/en/github/managing-security-vulnerabilities/about-security-alerts-for-vulnerable-dependencies for details. Note that vulnerability alerts have not been successfully tested on any GitHub Enterprise instance and may be unavailable in those settings."
 }
 
