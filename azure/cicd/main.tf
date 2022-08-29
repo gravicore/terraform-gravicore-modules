a59c22e6e276c6a320790daa76c6ef1467e8046d
Benjamin Leon <benjamin.leon@gravicore.io>
2022-08-29 14:24:00 -0700

    GDEV-258 Added base CICD module

---
+++ azure/cicd/main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_role_definition" "elevated" {
+  count = var.create ? 1 : 0
+  name  = join(var.delimiter, [var.stage_prefix, "elevated", "access"])
+  scope = azurerm_subscription.current.id
+
+  permissions {
+    actions     = ["*"]
+    not_actions = []
+  }
+
+  assignable_scopes = [data.azurerm_subscription.current[0].id]
+}
+
+resource "azurerm_storage_account" "cicd" {
+  count                    = var.create ? 1 : 0
+  name                     = join("", [var.namespace, var.environment, var.stage, "cicd", "artifacts"])
+  resource_group_name      = concat(azurerm_resource_group.default.*.name, [""])[0]
+  location                 = var.az_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = local.tags
+}
+
+resource "azurerm_storage_container" "cicd" {
+  count                 = var.create ? 1 : 0
+  name                  = join("", [var.namespace, var.environment, var.stage, "cicd", "artifacts"])
+  storage_account_name  = azurerm_storage_account.cicd.name
+  container_access_type = "blob"
+}
+
---
+++ azure/cicd/variables.tf
@@ -0,0 +1,142 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "name" {
+  type        = string
+  default     = "key-vault"
+  description = "The name of the module"
+}
+
+variable "terraform_module" {
+  type        = string
+  default     = "gravicore/terraform-gravicore-modules/azure/kv"
+  description = "The owner and name of the Terraform module"
+}
+
+variable "az_location" {
+  type        = string
+  default     = "westus"
+  description = "The Azure region to deploy module into"
+}
+
+variable "create" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating any resources"
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = ""
+  description = ""
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Platform Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Recommended
+
+variable "namespace" {
+  type        = string
+  default     = ""
+  description = "Namespace, which could be your organization abbreviation, client name, etc. (e.g. Gravicore 'grv', HashiCorp 'hc')"
+}
+
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "The isolated environment the module is associated with (e.g. Shared Services `shared`, Application `app`)"
+}
+
+variable "stage" {
+  type        = string
+  default     = ""
+  description = "The development stage (i.e. `dev`, `stg`, `prd`)"
+}
+
+variable "repository" {
+  type        = string
+  default     = "sf-dm-infra"
+  description = "The repository where the code referencing the module is stored"
+}
+
+# Optional
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional map of tags (e.g. business_unit, cost_center)"
+}
+
+variable "desc_prefix" {
+  type        = string
+  default     = "Gravicore:"
+  description = "The prefix to add to any descriptions attached to resources"
+}
+
+variable "environment_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace` and `environment`"
+}
+
+variable "stage_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace`, `environment` and `stage`"
+}
+
+variable "module_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace`, `environment`, `stage` and `name`"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `environment`, `stage`, `name`"
+}
+
+locals {
+  environment_prefix = coalesce(var.environment_prefix, join(var.delimiter, compact([var.namespace, var.environment])))
+  stage_prefix       = coalesce(var.stage_prefix, join(var.delimiter, compact([local.environment_prefix, var.stage])))
+  module_prefix      = coalesce(var.module_prefix, join(var.delimiter, compact([local.stage_prefix, var.name])))
+
+  business_tags = {
+    namespace          = var.namespace
+    environment        = var.environment
+    environment_prefix = local.environment_prefix
+  }
+  technical_tags = {
+    stage       = var.stage
+    module      = var.name
+    repository  = var.repository
+    az_location = var.az_location
+  }
+  automation_tags = {
+    terraform_module = var.terraform_module
+    stage_prefix     = local.stage_prefix
+    module_prefix    = local.module_prefix
+  }
+  security_tags = {}
+
+  tags = merge(
+    local.business_tags,
+    local.technical_tags,
+    local.automation_tags,
+    local.security_tags,
+    var.tags
+  )
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_subscription" "current" {}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
