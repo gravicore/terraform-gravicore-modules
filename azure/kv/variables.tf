0c1333e3c0244023b9d79da71cc43a2d08d03906
Benjamin Leon <benjamin.leon@gravicore.io>
2022-08-29 09:00:59 -0700

    GDEV-257 Added base key vault module

---
+++ azure/kv/variables.tf
@@ -0,0 +1,185 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "name" {
+  type        = string
+  default     = "resource-group"
+  description = "The name of the module"
+}
+
+variable "terraform_module" {
+  type        = string
+  default     = "gravicore/terraform-gravicore-modules/azure/rg"
+  description = "The owner and name of the Terraform module"
+}
+
+variable "az_location" {
+  type        = string
+  default     = "westus"
+  description = "The Azure region to deploy module into"
+}
+
+variable "create" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating any resources"
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = ""
+  description = ""
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Platform Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Recommended
+
+variable "namespace" {
+  type        = string
+  default     = "sf"
+  description = "Namespace, which could be your organization abbreviation, client name, etc. (e.g. Gravicore 'grv', HashiCorp 'hc')"
+}
+
+variable "environment" {
+  type        = string
+  default     = "dm"
+  description = "The isolated environment the module is associated with (e.g. Shared Services `shared`, Application `app`)"
+}
+
+variable "stage" {
+  type        = string
+  default     = "dev"
+  description = "The development stage (i.e. `dev`, `stg`, `prd`)"
+}
+
+variable "repository" {
+  type        = string
+  default     = "sf-dm-infra"
+  description = "The repository where the code referencing the module is stored"
+}
+
+# Optional
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional map of tags (e.g. business_unit, cost_center)"
+}
+
+variable "desc_prefix" {
+  type        = string
+  default     = "Gravicore:"
+  description = "The prefix to add to any descriptions attached to resources"
+}
+
+variable "environment_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace` and `environment`"
+}
+
+variable "stage_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace`, `environment` and `stage`"
+}
+
+variable "module_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace`, `environment`, `stage` and `name`"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `environment`, `stage`, `name`"
+}
+
+locals {
+  environment_prefix = coalesce(var.environment_prefix, join(var.delimiter, compact([var.namespace, var.environment])))
+  stage_prefix       = coalesce(var.stage_prefix, join(var.delimiter, compact([local.environment_prefix, var.stage])))
+  module_prefix      = coalesce(var.module_prefix, join(var.delimiter, compact([local.stage_prefix, var.name])))
+
+  business_tags = {
+    namespace          = var.namespace
+    environment        = var.environment
+    environment_prefix = local.environment_prefix
+  }
+  technical_tags = {
+    stage       = var.stage
+    module      = var.name
+    repository  = var.repository
+    az_location = var.az_location
+  }
+  automation_tags = {
+    terraform_module = var.terraform_module
+    stage_prefix     = local.stage_prefix
+    module_prefix    = local.module_prefix
+  }
+  security_tags = {}
+
+  tags = merge(
+    local.business_tags,
+    local.technical_tags,
+    local.automation_tags,
+    local.security_tags,
+    var.tags
+  )
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "sku_name" {
+  type        = string
+  default     = "standard"
+  description = "(Required) The Name of the SKU used for this Key Vault. Possible values are standard and premium."
+}
+
+variable "enabled_for_deployment" {
+  type        = bool
+  default     = false
+  description = "(Optional) Boolean flag to specify whether Azure Virtual Machines are permitted to retrieve certificates stored as secrets from the key vault. Defaults to false."
+}
+
+variable "enabled_for_disk_encryption" {
+  type        = bool
+  default     = false
+  description = "(Optional) Boolean flag to specify whether Azure Disk Encryption is permitted to retrieve secrets from the vault and unwrap keys. Defaults to false."
+}
+
+variable "enabled_for_template_deployment" {
+  type        = bool
+  default     = false
+  description = "(Optional) Boolean flag to specify whether Azure Resource Manager is permitted to retrieve secrets from the key vault. Defaults to false."
+}
+
+variable "enable_rbac_authorization" {
+  type        = bool
+  default     = false
+  description = "(Optional) Boolean flag to specify whether Azure Key Vault uses Role Based Access Control (RBAC) for authorization of data actions. Defaults to false."
+}
+
+variable "purge_protection_enabled" {
+  type = bool
+  default = false
+  description = "(Optional) Is Purge Protection enabled for this Key Vault? Defaults to false."
+}
+
+variable "public_network_access_enabled" {
+  type = bool
+  default = true
+  description = "(Optional) Whether public network access is allowed for this Key Vault. Defaults to true."
+}
+
+variable "soft_delete_retention_days" {
+  type = number
+  default = 14
+  description = "(Optional) The number of days that items should be retained for once soft-deleted. This value can be between 7 and 90 (the default) days."
+}
