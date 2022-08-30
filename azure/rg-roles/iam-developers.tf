b2a003c5acb31bdc1fbeaa99acaa53b246de51d3
Benjamin Leon <benjamin.leon@gravicore.io>
2022-08-30 12:58:09 -0700

    GDEV-248 role naming update

--- azure/rg-roles/iam-developers.tf
+++ azure/rg-roles/iam-developers.tf
@@ -1,6 +1,6 @@
 resource "azurerm_role_definition" "developer" {
   count = var.create ? 1 : 0
-  name  = join(var.delimiter, [var.namespace, "developer", ])
+  name  = join(var.delimiter, [var.namespace, var.environment, "developer"])
   scope = data.azurerm_subscription.current.id
 
   permissions {
--- azure/rg-roles/iam-devops.tf
+++ azure/rg-roles/iam-devops.tf
@@ -1,6 +1,6 @@
 resource "azurerm_role_definition" "devops" {
   count = var.create ? 1 : 0
-  name  = join(var.delimiter, [var.namespace, "devops", "access"])
+  name  = join(var.delimiter, [var.namespace, var.environment, "devops"])
   scope = data.azurerm_subscription.current.id
 
   permissions {
