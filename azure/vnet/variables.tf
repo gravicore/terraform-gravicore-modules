07e208f5e8dfca94bc05479a1f72eb5c7848d176
Benjamin Leon <benjamin.leon@gravicore.io>
2022-08-30 09:44:54 -0700

    GDEV-247 File structure fix

---
+++ azure/vnet/variables.tf
@@ -0,0 +1,201 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "name" {
+  type        = string
+  default     = "vnet"
+  description = "The name of the module"
+}
+
+variable "terraform_module" {
+  type        = string
+  default     = "gravicore/terraform-gravicore-modules/azure/vnet"
+  description = "The owner and name of the Terraform module"
+}
+
+variable "az_location" {
+  type        = string
+  default     = "us-east-1"
+  description = "The Azure region to deploy module into"
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "dm-sandbox"
+  description = "The name of the Azure resource group"
+}
+
+variable "create" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating any resources"
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Platform Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Recommended
+
+variable "namespace" {
+  type        = string
+  default     = "sf"
+  description = "Namespace, which could be your organization abbreviation, client name, etc. (e.g. Gravicore 'grv', HashiCorp 'hc')"
+}
+
+variable "environment" {
+  type        = string
+  default     = "dm"
+  description = "The isolated environment the module is associated with (e.g. Shared Services `shared`, Application `app`)"
+}
+
+variable "stage" {
+  type        = string
+  default     = ""
+  description = "The development stage (i.e. `dev`, `stg`, `prd`)"
+}
+
+variable "repository" {
+  type        = string
+  default     = ""
+  description = "The repository where the code referencing the module is stored"
+}
+
+variable "account_id" {
+  type        = string
+  default     = ""
+  description = "The Azure Account ID that contains the calling entity"
+}
+
+variable "master_account_id" {
+  type        = string
+  default     = ""
+  description = "The Master Azure Account ID that owns the associate Azure account"
+}
+
+# Optional
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional map of tags (e.g. business_unit, cost_center)"
+}
+
+variable "desc_prefix" {
+  type        = string
+  default     = "Gravicore:"
+  description = "The prefix to add to any descriptions attached to resources"
+}
+
+variable "environment_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace` and `environment`"
+}
+
+variable "stage_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace`, `environment` and `stage`"
+}
+
+variable "module_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace`, `environment`, `stage` and `name`"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `environment`, `stage`, `name`"
+}
+
+locals {
+  environment_prefix = coalesce(var.environment_prefix, join(var.delimiter, compact([var.namespace, var.environment])))
+  stage_prefix       = coalesce(var.stage_prefix, join(var.delimiter, compact([local.environment_prefix, var.stage])))
+  module_prefix      = coalesce(var.module_prefix, join(var.delimiter, compact([local.stage_prefix, var.name])))
+
+  business_tags = {
+    namespace          = var.namespace
+    environment        = var.environment
+    environment_prefix = local.environment_prefix
+  }
+  technical_tags = {
+    stage             = var.stage
+    module            = var.name
+    repository        = var.repository
+    master_account_id = var.master_account_id
+    # account_id        = local.account_id
+    az_location = var.az_location
+  }
+  automation_tags = {
+    terraform_module = var.terraform_module
+    stage_prefix     = local.stage_prefix
+    module_prefix    = local.module_prefix
+  }
+  security_tags = {}
+
+  tags = merge(
+    local.business_tags,
+    local.technical_tags,
+    local.automation_tags,
+    local.security_tags,
+    var.tags
+  )
+}
+
+
+variable "vnet_cidr_block" {
+  default     = ["10.0.0.0/16"]
+  description = "(Required) The address space that is used the virtual network. You can supply more than one address space."
+}
+
+variable "dns_servers" {
+  type        = list(any)
+  default     = null
+  description = "(Optional) List of IP addresses of DNS servers"
+}
+
+variable "edge_zone" {
+  type        = string
+  default     = null
+  description = "(Optional) Specifies the Edge Zone within the Azure Region where this Virtual Network should exist. Changing this forces a new Virtual Network to be created."
+}
+
+variable "flow_timeout_in_minutes" {
+  type        = number
+  default     = null
+  description = "(Optional) The flow timeout in minutes for the Virtual Network, which is used to enable connection tracking for intra-VM flows. Possible values are between 4 and 30 minutes."
+}
+
+variable "az_max_count" {
+  type        = number
+  default     = 2
+  description = "Sets the maximum number of Availability Zones (up to 3)"
+}
+
+variable "bgp_community" {
+  type        = string
+  default     = null
+  description = "(Optional) The BGP community attribute in format <as-number>:<community-value>."
+}
+
+variable "vpc_public_subnets" {
+  type        = list(string)
+  default     = []
+  description = "The public subnets of the VPC to use"
+}
+
+variable "vpc_private_subnets" {
+  type        = list(string)
+  default     = []
+  description = "The private subnets of the VPC to use"
+}
+
+variable "vpc_internal_subnets" {
+  type        = list(string)
+  default     = null
+  description = "The internal subnets (no NAT access) of the VPC to use"
+}
