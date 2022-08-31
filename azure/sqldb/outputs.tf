292ad526779279e0808515554387ce902fb70af5
Benjamin Leon <benjamin.leon@gravicore.io>
2022-08-31 10:12:01 -0700

    GDEV-249 Added outputs

---
+++ azure/sqldb/outputs.tf
@@ -0,0 +1,15 @@
+output "sql_server_id" {
+  value = concat(azurerm_mssql_server.default.*.id, [""])[0]
+}
+
+output "sql_server_FQDN" {
+  value = concat(azurerm_mssql_server.default.*.fully_qualified_domain_name, [""])[0]
+}
+
+output "sql_database_id" {
+  value = concat(azurerm_mssql_database.default.*.id)[0]
+}
+
+output "sql_database_id" {
+  value = concat(azurerm_mssql_database.default.*.name)[0]
+}
