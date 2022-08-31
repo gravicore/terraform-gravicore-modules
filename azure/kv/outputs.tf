653d9b0954bb6ebfa56958664a6a33aa525940b6
Benjamin Leon <benjamin.leon@gravicore.io>
2022-08-31 15:13:37 -0700

    GDEV-257 output updates

--- azure/kv/outputs.tf
+++ azure/kv/outputs.tf
@@ -8,8 +8,16 @@ output "vm_key_id" {
   value = concat(azurerm_key_vault_key.vm_key.*.id, [""])[0]
 }
 
+output "vm_key_name" {
+  value = concat(azurerm_key_vault_key.vm_key.*.name, [""])[0]
+}
+
 output "sa_key_id" {
   value = concat(azurerm_key_vault_key.sa_key.*.id, [""])[0]
+}
+
+output "sa_key_name" {
+  value = concat(azurerm_key_vault_key.sa_key.*.name, [""])[0]
 }
 
 # Certificate outputs
