894ed9ff1ea7d0382003fa6274142d1a3e6f1ded
Benjamin Leon <benjamin.leon@gravicore.io>
2022-08-31 09:27:48 -0700

    GDEV-249 Update to latest file structure

---
+++ azure/sql-db/main.tf
@@ -0,0 +1,36 @@
+resource "azurerm_mssql_server" "default" {
+  count               = var.create ? 1 : 0
+  name                = join(var.delimiter, [local.stage_prefix, "sql"])
+  resource_group_name = local.resource_group_name
+  location            = local.az_location
+  tags                = local.tags
+
+  version                      = var.sql_server_version
+  administrator_login          = var.sql_server_administrator_login
+  administrator_login_password = var.sql_server_administrator_login_password
+  minimum_tls_version          = "1.2"
+}
+
+resource "azurerm_mssql_database" "default" {
+  count               = var.create ? 1 : 0
+  name                = join(var.delimiter, [local.stage_prefix, "sqldb"])
+  location            = local.az_location
+  resource_group_name = local.resource_group_name
+  tags                = local.tags
+
+  server_id                   = concat(azurerm_mssql_server.default.*.id, [""])[0]
+  auto_pause_delay_in_minutes = var.auto_pause_delay_in_minutes
+  create_mode                 = var.create_mode
+  source_database_id          = var.create_mode != "DEFAULT" ? var.source_database_id : null
+  collation                   = var.collation
+  geo_backup_enabled          = var.geo_backup_enabled
+  ledger_enabled              = var.ledger_enabled
+  license_type                = var.license_type
+  max_size_gb                 = var.max_size_gb
+  min_capacity                = var.min_capacity
+  restore_point_in_time       = var.create_mode == "PointInTimeRestore" ? var.restore_point_in_time : null
+  recover_database_id         = var.create_mode == "Recovery" ? var.recover_database_id : null
+  restore_dropped_database_id = var.create_mode == "Restore" ? var.restore_dropped_database_id : null
+  read_replica_count          = var.sku_name == "HS_Gen4_1" ? var.read_replica_count : null
+  sku_name                    = var.sku_name
+}
---
+++ azure/sql-db/variables.tf
@@ -0,0 +1,247 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "name" {
+  type        = string
+  default     = "key-vault"
+  description = "The name of the module"
+}
+
+variable "terraform_module" {
+  type        = string
+  default     = "gravicore/terraform-gravicore-modules/azure/kv"
+  description = "The owner and name of the Terraform module"
+}
+
+variable "az_location" {
+  type        = string
+  default     = "westus"
+  description = "The Azure region to deploy module into"
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = ""
+  description = "The name of the Azure resource group"
+}
+
+variable "create" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating any resources"
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Platform Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Recommended
+
+variable "namespace" {
+  type        = string
+  default     = ""
+  description = "Namespace, which could be your organization abbreviation, client name, etc. (e.g. Gravicore 'grv', HashiCorp 'hc')"
+}
+
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "The isolated environment the module is associated with (e.g. Shared Services `shared`, Application `app`)"
+}
+
+variable "stage" {
+  type        = string
+  default     = ""
+  description = "The development stage (i.e. `dev`, `stg`, `prd`)"
+}
+
+variable "repository" {
+  type        = string
+  default     = "sf-dm-infra"
+  description = "The repository where the code referencing the module is stored"
+}
+
+# Optional
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional map of tags (e.g. business_unit, cost_center)"
+}
+
+variable "desc_prefix" {
+  type        = string
+  default     = "Gravicore:"
+  description = "The prefix to add to any descriptions attached to resources"
+}
+
+variable "environment_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace` and `environment`"
+}
+
+variable "stage_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace`, `environment` and `stage`"
+}
+
+variable "module_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace`, `environment`, `stage` and `name`"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `environment`, `stage`, `name`"
+}
+
+locals {
+  environment_prefix = coalesce(var.environment_prefix, join(var.delimiter, compact([var.namespace, var.environment])))
+  stage_prefix       = coalesce(var.stage_prefix, join(var.delimiter, compact([local.environment_prefix, var.stage])))
+  module_prefix      = coalesce(var.module_prefix, join(var.delimiter, compact([local.stage_prefix, var.name])))
+
+  business_tags = {
+    namespace          = var.namespace
+    environment        = var.environment
+    environment_prefix = local.environment_prefix
+  }
+  technical_tags = {
+    stage       = var.stage
+    module      = var.name
+    repository  = var.repository
+    az_location = var.az_location
+  }
+  automation_tags = {
+    terraform_module = var.terraform_module
+    stage_prefix     = local.stage_prefix
+    module_prefix    = local.module_prefix
+  }
+  security_tags = {}
+
+  tags = merge(
+    local.business_tags,
+    local.technical_tags,
+    local.automation_tags,
+    local.security_tags,
+    var.tags
+  )
+}
+
+data "azurerm_client_config" "current" {}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "sql_server_version" {
+  type        = string
+  default     = "12.0"
+  description = "(Required) The version for the new server. Valid values are: 2.0 (for v11 server) and 12.0 (for v12 server)."
+}
+
+variable "sql_server_administrator_login" {
+  type        = string
+  default     = null
+  description = "(Optional) The administrator login name for the new server. Required unless azuread_authentication_only in the azuread_administrator block is true. When omitted, Azure will generate a default username which cannot be subsequently changed. Changing this forces a new resource to be created."
+}
+
+variable "sql_server_administrator_login_password" {
+  type        = string
+  default     = ""
+  description = "(Optional) The password associated with the administrator_login user. Needs to comply with Azure's Password Policy. Required unless azuread_authentication_only in the azuread_administrator block is true."
+}
+
+variable "minimum_tls_version" {
+  type        = string
+  default     = "1.2"
+  description = "(Optional) The Minimum TLS Version for all SQL Database and SQL Data Warehouse databases associated with the server. Valid values are: 1.0, 1.1 , 1.2 and Disabled. Defaults to 1.2."
+}
+
+variable "auto_pause_delay_in_minutes" {
+  type        = number
+  default     = 5
+  description = "(Optional) Time in minutes after which database is automatically paused. A value of -1 means that automatic pause is disabled. This property is only settable for General Purpose Serverless databases."
+}
+
+variable "create_mode" {
+  type        = string
+  default     = "DEFAULT"
+  description = "(Optional) The create mode of the database. Possible values are Copy, Default, OnlineSecondary, PointInTimeRestore, Recovery, Restore, RestoreExternalBackup, RestoreExternalBackupSecondary, RestoreLongTermRetentionBackup and Secondary."
+}
+
+variable "source_database_id" {
+  type        = string
+  default     = ""
+  description = "(Optional) The ID of the source database from which to create the new database. This should only be used for databases with create_mode values that use another database as reference. Changing this forces a new resource to be created."
+}
+
+variable "collation" {
+  type        = string
+  default     = "SQL_Latin1_General_CP1_CI_AS"
+  description = "(Optional) Specifies the collation of the database. Changing this forces a new resource to be created."
+}
+
+variable "geo_backup_enabled" {
+  type        = bool
+  default     = false
+  description = "(Optional) A boolean that specifies if the Geo Backup Policy is enabled."
+}
+
+variable "ledger_enabled" {
+  type        = bool
+  default     = false
+  description = "(Optional) A boolean that specifies if this is a ledger database. Defaults to false. Changing this forces a new resource to be created."
+}
+
+variable "license_type" {
+  type        = string
+  default     = "LicenseIncluded"
+  description = "(Optional) Specifies the license type applied to this database. Possible values are LicenseIncluded and BasePrice."
+}
+
+variable "max_size_gb" {
+  type        = number
+  default     = 100
+  description = "(Optional) The max size of the database in gigabytes."
+}
+
+variable "min_capacity" {
+  type        = number #? Is this correct?
+  default     = 5
+  description = "(Optional) Minimal capacity that database will always have allocated, if not paused. This property is only settable for General Purpose Serverless databases."
+}
+
+variable "restore_point_in_time" {
+  type        = string
+  default     = ""
+  description = "(Required) Specifies the point in time (ISO8601 format) of the source database that will be restored to create the new database. This property is only settable for create_mode= PointInTimeRestore databases."
+}
+
+variable "recover_database_id" {
+  type        = string
+  default     = ""
+  description = "(Optional) The ID of the database to be recovered. This property is only applicable when the create_mode is Recovery."
+}
+
+variable "restore_dropped_database_id" {
+  type        = string
+  default     = ""
+  description = "(Optional) The ID of the database to be restored. This property is only applicable when the create_mode is Restore."
+}
+
+variable "read_replica_count" {
+  type        = number
+  default     = 0
+  description = "(Optional) The number of readonly secondary replicas associated with the database to which readonly application intent connections may be routed. This property is only settable for Hyperscale edition databases."
+}
+
+variable "sku_name" {
+  type        = string
+  default     = "GP_S_Gen5_2"
+  description = "(Optional) Specifies the name of the SKU used by the database. For example, GP_S_Gen5_2,HS_Gen4_1,BC_Gen5_2, ElasticPool, Basic,S0, P2 ,DW100c, DS100. Changing this from the HyperScale service tier to another service tier will force a new resource to be created."
+}
