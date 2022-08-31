fcd19665677261e8259252cddccba492629ddfcd
Benjamin Leon <benjamin.leon@gravicore.io>
2022-08-31 15:31:10 -0700

    GDEV-244 Output fixes

--- azure/sa/outputs.tf
+++ azure/sa/outputs.tf
@@ -1,14 +1,14 @@
 output "storage_account_name" {
   description = "The name of the Storage Account"
-  value       = concat(azurerm_storage_account.default[0].name, [""])[0]
+  value       = concat(azurerm_storage_account.default.*.name, [""])[0]
 }
 
 output "storage_account_kind" {
   description = "The kind of the Storage Account"
-  value       = concat(azurerm_storage_account.default[0].account_kind, [""])[0]
+  value       = concat(azurerm_storage_account.default.*.account_kind, [""])[0]
 }
 
 output "storage_account_type" {
   description = "The tier of the Storage Account"
-  value       = concat(azurerm_storage_account.default[0].account_tier, [""])[0]
+  value       = concat(azurerm_storage_account.default.*.account_tier, [""])[0]
 }
