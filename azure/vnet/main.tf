e0ef95fc864d71459869bec6e5f3a78234e3809a
Benjamin Leon <benjamin.leon@gravicore.io>
2022-09-01 12:53:47 -0700

    GDEV-247 naming update

--- azure/vnet/main.tf
+++ azure/vnet/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_virtual_network" "default" {
 
 resource "azurerm_subnet" "public" {
   for_each             = var.create ? toset(local.vnet_public_subnets) : []
-  name                 = join(var.delimiter, [local.module_prefix, "public", index(local.vnet_public_subnets, each.key) + 1])
+  name                 = join(var.delimiter, [local.stage_prefix, "snet", "public", index(local.vnet_public_subnets, each.key) + 1])
   resource_group_name  = var.resource_group_name
   virtual_network_name = concat(azurerm_virtual_network.default.*.name, [""])[0]
   address_prefixes     = [each.key]
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "public" {
 
 resource "azurerm_subnet" "private" {
   for_each             = var.create ? toset(local.vnet_private_subnets) : []
-  name                 = join(var.delimiter, [local.module_prefix, "private", index(local.vnet_private_subnets, each.key) + 1])
+  name                 = join(var.delimiter, [local.stage_prefix, "snet", "private", index(local.vnet_private_subnets, each.key) + 1])
   resource_group_name  = var.resource_group_name
   virtual_network_name = concat(azurerm_virtual_network.default.*.name, [""])[0]
   address_prefixes     = [each.key]
