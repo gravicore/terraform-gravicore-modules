b61312dd7d0a7325db609c79d692e91f0a7b2070
Benjamin Leon <benjamin.leon@gravicore.io>
2022-09-01 13:01:13 -0700

    GDEV-248 Formatting

--- azure/rg/state-sa.tf
+++ azure/rg/state-sa.tf
@@ -1,12 +1,12 @@
 resource "azurerm_storage_account" "tfstate" {
-  count                    = var.create ? 1 : 0
-  name                     = join("", [var.namespace, var.environment, var.stage, "tf", "state"])
-  resource_group_name      = concat(azurerm_resource_group.default.*.name, [""])[0]
-  location                 = var.az_location
+  count               = var.create ? 1 : 0
+  name                = join("", [var.namespace, var.environment, var.stage, "tf", "state"])
+  resource_group_name = concat(azurerm_resource_group.default.*.name, [""])[0]
+  location            = var.az_location
+  tags                = local.tags
+
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-  tags = local.tags
 
   depends_on = [
     azurerm_resource_group.default[0]
@@ -14,8 +14,9 @@ resource "azurerm_storage_account" "tfstate" {
 }
 
 resource "azurerm_storage_container" "tfstate" {
-  count                 = var.create ? 1 : 0
-  name                  = join(var.delimiter, [var.namespace, var.environment, var.stage, "tf", "state"])
+  count = var.create ? 1 : 0
+  name  = join(var.delimiter, [var.namespace, var.environment, var.stage, "tf", "state"])
+
   storage_account_name  = concat(azurerm_storage_account.tfstate.*.name, [""])[0]
   container_access_type = "blob"
 }
