92c70dd5920f2fe09a9b4eca23470a59ff8be93c
Benjamin Leon <benjamin.leon@gravicore.io>
2022-10-06 15:38:35 -0700

    GDEV-265 Removed for import

--- azure/boomi/main.tf
+++ azure/boomi/main.tf
@@ -1,8 +1,8 @@
-resource "azurerm_linux_virtual_machine" "default" {
-  name                = join(var.delimiter, [var.namespace, var.environment, var.stage, var.az_location, var.name, "vm", "boomi"])
-  resource_group_name = var.resource_group_name
-  location            = var.az_location
+# resource "azurerm_linux_virtual_machine" "default" {
+#   name                = join(var.delimiter, [var.namespace, var.environment, var.stage, var.az_location, var.name, "vm", "boomi"])
+#   resource_group_name = var.resource_group_name
+#   location            = var.az_location
 
-  size           = "Standard_B2"
-  admin_username = "admin"
-}
+#   size           = "Standard_B2"
+#   admin_username = "admin"
+# }
