8f429baef436b73e919dde8df0ffa539fad13a2d
Benjamin Leon <benjamin.leon@gravicore.io>
2022-10-06 15:56:14 -0700

    GDEV-265 Added count variable

--- azure/boomi/main.tf
+++ azure/boomi/main.tf
@@ -1,4 +1,5 @@
 resource "azurerm_linux_virtual_machine" "default" {
+  count               = var.create ? 1 : 0
   name                = join(var.delimiter, [var.namespace, var.environment, var.stage, var.az_location, var.name, "vm", "boomi"])
   resource_group_name = var.resource_group_name
   location            = var.az_location
