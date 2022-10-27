3b412bb22bcbfcfd047e91f949a170061d2feff0
Benjamin Leon <benjamin.leon@gravicore.io>
2022-10-27 14:46:12 -0700

    GDEV-265 Added base module settings

--- azure/boomi/main.tf
+++ azure/boomi/main.tf
@@ -1,16 +1,27 @@
 resource "azurerm_linux_virtual_machine" "default" {
-  count               = var.create ? 1 : 0
-  name                = join(var.delimiter, [var.namespace, var.environment, var.stage, var.az_location, var.name, "vm", "boomi"])
-  resource_group_name = var.resource_group_name
-  location            = var.az_location
-  tags                = local.tags
+  count                 = var.create ? 1 : 0
+  name                  = join(var.delimiter, [var.namespace, var.environment, var.stage, var.az_location, var.name, "vm", "boomi"])
+  resource_group_name   = var.resource_group_name
+  location              = var.az_location
+  tags                  = local.tags
+  network_interface_ids = azurerm_network_interface.default.*.id
 
-  size           = "Standard_B2"
-  admin_username = "admin"
+  size            = var.size
+  admin_username  = var.admin_username
+  admin_password  = var.admin_password
+  license_type    = var.license_type
+  source_image_id = var.source_image_id
 
   os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    caching              = var.os_disk_caching
+    storage_account_type = var.os_disk_account_type
+  }
+
+  source_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
   }
 }
 
@@ -27,13 +38,32 @@ resource "azurerm_network_interface" "default" {
   enable_accelerated_networking = var.enable_accelerated_networking
   internal_dns_name_label       = var.internal_dns_name_label
   ip_configuration {
-    name                                               = join(var.delimiter, [var.namespace, var.environment, var.stage, var.az_location, var.name, "vm", "ipconfig"])
+    name                                               = join(var.delimiter, [var.namespace, var.environment, var.stage, var.az_location, var.name, "vm", "ip-config"])
     subnet_id                                          = var.private_ip_address_version != "IPv4" ? null : var.subnet_id
     gateway_load_balancer_frontend_ip_configuration_id = var.gateway_load_balancer_frontend_ip_configuration_id
     private_ip_address_version                         = var.private_ip_address_version
     private_ip_address_allocation                      = var.private_ip_address_allocation
     private_ip_address                                 = var.private_ip_address_allocation == "static" ? null : var.private_ip_address
-    public_ip_address_id                               = var.public_ip_address_id
+    public_ip_address_id                               = var.create_public_ip == null ? null : azurerm_public_ip.default[0].id
     primary                                            = var.primary
   }
+}
+
+resource "azurerm_public_ip" "default" {
+  count               = var.create && var.create_public_ip ? 1 : 0
+  name                = join(var.delimiter, [var.namespace, var.environment, var.stage, var.az_location, var.name, "vm", "public-ip"])
+  resource_group_name = var.resource_group_name
+  location            = var.az_location
+  tags                = local.tags
+
+  allocation_method       = var.allocation_method
+  zones                   = var.zones
+  domain_name_label       = var.domain_name_label
+  edge_zone               = var.edge_zone
+  idle_timeout_in_minutes = var.idle_timeout_in_minutes
+  ip_tags                 = var.ip_tags
+  ip_version              = var.ip_version
+  reverse_fqdn            = var.reverse_fqdn
+  sku                     = var.sku_tier == "Global" ? "Standard" : var.sku
+  sku_tier                = var.sku_tier
 }
--- azure/boomi/variables.tf
+++ azure/boomi/variables.tf
@@ -210,3 +210,81 @@ variable "primary" {
   default     = true
   description = "(Optional) Is this the Primary IP Configuration? Must be true for the first ip_configuration when multiple are specified. Defaults to false [overriden to true]."
 }
+
+variable "size" {
+  type        = string
+  default     = "Standard_B2"
+  description = "(Required) The SKU which should be used for this Virtual Machine, such as Standard_F2."
+}
+
+variable "license_type" {
+  type        = string
+  default     = null
+  description = "(Optional) Specifies the BYOL Type for this Virtual Machine. Possible values are RHEL_BYOS and SLES_BYOS."
+}
+
+variable "admin_password" {
+  type        = string
+  default     = null
+  description = "(Optional) The Password which should be used for the local-administrator on this Virtual Machine. Changing this forces a new resource to be created."
+}
+
+variable "admin_username" {
+  type        = string
+  default     = "admin"
+  desciprtion = "(Required) The username of the local administrator used for the Virtual Machine. Changing this forces a new resource to be created."
+}
+
+variable "assign_public_ip" {
+  type        = bool
+  default     = false
+  description = "Whether or not to assign a public IP address to this NIC"
+}
+
+variable "source_image_id" {
+  type        = string
+  default     = ""
+  description = "(Optional) The ID of the Image which this Virtual Machine should be created from. Changing this forces a new resource to be created. Possible Image ID types include Image IDs, Shared Image IDs, Shared Image Version IDs, Community Gallery Image IDs, Community Gallery Image Version IDs, Shared Gallery Image IDs and Shared Gallery Image Version IDs."
+}
+
+variable "create_public_ip" {
+  type        = bool
+  default     = false
+  description = "A bool flag to enable the creation of a public ip address"
+}
+
+variable "os_disk_caching" {
+  type        = string
+  default     = "ReadWrite"
+  description = "(Required) The Type of Caching which should be used for the Internal OS Disk. Possible values are None, ReadOnly and ReadWrite."
+}
+
+variable "os_disk_account_type" {
+  type        = string
+  default     = "Standard_LRS"
+  description = "(Required) The Type of Storage Account which should back this the Internal OS Disk. Possible values are Standard_LRS, StandardSSD_LRS, Premium_LRS, StandardSSD_ZRS and Premium_ZRS. Changing this forces a new resource to be created."
+}
+
+variable "image_publisher" {
+  type = string
+  default = "Canonical"
+  description = "(Optional) Specifies the publisher of the image used to create the virtual machines."
+}
+
+variable "image_offer" {
+  type = string
+  default = "UbuntuServer"
+  description = "(Optional) Specifies the offer of the image used to create the virtual machines."
+}
+
+variable "image_sku" {
+  type = string
+  default = "16.04-LTS"
+  description = "(Optional) Specifies the SKU of the image used to create the virtual machines."
+}
+
+variable "image_version" {
+  type = string
+  default = "latest"
+  description = "(Optional) Specifies the version of the image used to create the virtual machines."
+}
