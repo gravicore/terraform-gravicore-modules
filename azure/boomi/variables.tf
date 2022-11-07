6c140cc68001d90211c04bb618e33fcc54ce7a31
Benjamin Leon <benjamin.leon@gravicore.io>
2022-11-07 15:33:07 -0800

    GDEV-265 Variable updates

--- azure/boomi/variables.tf
+++ azure/boomi/variables.tf
@@ -308,31 +308,31 @@ variable "idle_timeout_in_minutes" {
 }
 
 variable "ip_version" {
-  type = string
-  default = "IPv4"
+  type        = string
+  default     = "IPv4"
   description = "(Optional) The IP Version to use, IPv6 or IPv4."
 }
 
 variable "ip_tags" {
-  type = list(any)
-  default = null
+  type        = map(any)
+  default     = [""]
   description = "value"
 }
 
 variable "sku_tier" {
-  type = string
-  default = "Regional"
+  type        = string
+  default     = "Regional"
   description = "(Optional) The SKU Tier that should be used for the Public IP. Possible values are Regional and Global. Defaults to Regional."
 }
 
 variable "sku" {
-  type = string
-  default = "Basic"
+  type        = string
+  default     = "Basic"
   description = "(Optional) The SKU of the Public IP. Accepted values are Basic and Standard. Defaults to Basic."
 }
 
 variable "reverse_fqdn" {
-  type = string
-  default = null
+  type        = string
+  default     = null
   description = "(Optional) A fully qualified domain name that resolves to this public IP address. If the reverseFqdn is specified, then a PTR DNS record is created pointing from the IP address in the in-addr.arpa domain to the reverse FQDN."
 }
