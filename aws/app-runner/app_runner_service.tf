ebe6d30a5bcfec1a148c4db7900d24b70f1d1967
alisonchicoria <alison.chicoria@gravicore.io>
2023-06-13 14:02:39 -0300

    DEVOPS-1096 testing dependency

--- aws/app-runner/app_runner_service.tf
+++ aws/app-runner/app_runner_service.tf
@@ -150,7 +150,7 @@ resource "aws_apprunner_service" "app_runner_service" {
 
 resource "aws_apprunner_auto_scaling_configuration_version" "auto_scaling_configuration" {
 
-  depends_on = [aws_apprunner_service.app_runner_service]
+  #depends_on = [aws_apprunner_service.app_runner_service]
 
   auto_scaling_configuration_name = "${local.module_prefix}-auto-scaling-configuration"
 
