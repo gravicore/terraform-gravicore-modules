b31200214dc01900f0c9695bb52c42fc8cfb95f7
alisonchicoria <alison.chicoria@gravicore.io>
2023-06-15 10:26:39 -0300

    DEVOPS-1118 fixed version again

--- aws/account/modules/terraform-backend/main.tf
+++ aws/account/modules/terraform-backend/main.tf
@@ -1,7 +1,12 @@
 terraform {
   required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "<= 4.67.0"
+    }
+  }
 }
-
 provider "aws" {
   assume_role {
     role_arn = var.aws_role
