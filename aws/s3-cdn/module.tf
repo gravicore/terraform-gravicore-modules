57d71c931f6c5b0198740db8f4ebd59fe47b79c5
Alison Chicoria <alisonchicoria@cloud.upwork.com>
2023-06-28 13:12:46 -0300

    Revert module to version 2.70

--- aws/s3-cdn/module.tf
+++ aws/s3-cdn/module.tf
@@ -3,8 +3,8 @@ terraform {
   required_version = "~> 0.13.0"
 
   required_providers {
-    aws      = "4.67.0"
-    template = ">= 2.0"
+    aws      = "<=2.70.4"
+    template = ">= 2.0."
     local    = ">= 1.2"
     null     = ">= 2.0"
   }
