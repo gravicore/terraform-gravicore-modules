078637616fad7381baa44d21f2c360a71cbbd724
Alison Chicoria <alisonchicoria@cloud.upwork.com>
2023-06-27 21:18:21 -0300

    Adjust version s3-cdn

--- aws/s3-cdn/module.tf
+++ aws/s3-cdn/module.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = "~> 0.13.0"
 
   required_providers {
-    aws      = ">= 2.0"
+    aws      = "4.67.0"
     template = ">= 2.0"
     local    = ">= 1.2"
     null     = ">= 2.0"
