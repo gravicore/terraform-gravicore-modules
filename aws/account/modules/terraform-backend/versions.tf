29de4c7e001576272b9227b4c2eb7e211e20187f
Jonathan Engene <jonathan.engene@gravicore.io>
2023-07-13 15:35:57 -0700

    DEVOPS-1116 provider update

--- aws/account/modules/terraform-backend/versions.tf
+++ aws/account/modules/terraform-backend/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "<= 4.67.0"
+      version = ">= 2.5.0"
     }
   }
   required_version = ">= 0.13"
--- aws/ds/versions.tf
+++ aws/ds/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      aws      = ">= 2.26"
     }
     random = {
       source = "hashicorp/random"
