63a0561f1e6f320b934b76c58d810b0521cdcd4c
Alison Chicoria <alisonchicoria@cloud.upwork.com>
2023-07-17 20:25:48 -0300

    Adjust modules

--- aws/vpc-dns/module.tf
+++ aws/vpc-dns/module.tf
@@ -1,6 +1,13 @@
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version      = ">= 2.26"
+    }
+  }
   required_version = ">= 0.13"
 }
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Module Standard Variables
