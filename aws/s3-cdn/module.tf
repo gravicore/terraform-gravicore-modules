9afe5fcd7a37a16207f1ada7fdf53ea310727c00
Alison Chicoria <alisonchicoria@cloud.upwork.com>
2023-07-19 11:47:57 -0300

    Adjust s3 cdn module

--- aws/s3-cdn/module.tf
+++ aws/s3-cdn/module.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.0"
+      version = "<= 4.67.0"
     }
     template = ">= 2.0"
     local    = ">= 1.2"
