750166edccfe37069e45d49b9a5df2d26a90a28f
Kagan Mersin <kagan.mersin@gmail.com>
2023-09-12 23:56:10 +0300

    add resource group module

---
+++ azure/rg/outputs.tf
@@ -0,0 +1,4 @@
+output "resource_group_id" {
+  value = azurerm_resource_group.default[*].id
+}
+
---
+++ azure/rg/variables.tf
@@ -0,0 +1,149 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+variable "name" {
+  type        = string
+  default     = "rg"
+  description = "The name of the module"
+}
+
+variable "terraform_module" {
+  type        = string
+  default     = "gravicore/terraform-gravicore-modules/azure/vnet"
+  description = "The owner and name of the Terraform module"
+}
+
+variable "region" {
+  type        = string
+  default     = ""
+  description = "The Azure region to deploy module into"
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = ""
+  description = "The name of the Azure resource group"
+}
+
+variable "create" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating any resources"
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Platform Standard Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Recommended
+
+variable "namespace" {
+  type        = string
+  default     = ""
+  description = "Namespace, which could be your organization abbreviation, client name, etc. (e.g. Gravicore 'grv', HashiCorp 'hc')"
+}
+
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "The isolated environment the module is associated with (e.g. Shared Services `shared`, Application `app`)"
+}
+
+variable "stage" {
+  type        = string
+  default     = ""
+  description = "The development stage (i.e. `dev`, `stg`, `prd`)"
+}
+
+variable "application" {
+  type        = string
+  default     = ""
+  description = "The application name (i.e. `apex`, `portal`)"
+}
+
+variable "repository" {
+  type        = string
+  default     = ""
+  description = "The repository where the code referencing the module is stored"
+}
+
+# Optional
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional map of tags (e.g. business_unit, cost_center)"
+}
+
+variable "desc_prefix" {
+  type        = string
+  default     = "gravicore:"
+  description = "The prefix to add to any descriptions attached to resources"
+}
+
+variable "environment_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace` and `environment`"
+}
+
+variable "stage_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace`, `environment` and `stage`"
+}
+
+variable "module_prefix" {
+  type        = string
+  default     = ""
+  description = "Concatenation of `namespace`, `environment`, `stage`, `application`, `region` and `name`"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `environment`, `stage`, `application`, `region` and `name`"
+}
+
+locals {
+  environment_prefix = coalesce(var.environment_prefix, join(var.delimiter, compact([var.namespace, var.environment])))
+  stage_prefix       = coalesce(var.stage_prefix, join(var.delimiter, compact([local.environment_prefix, var.stage])))
+  module_prefix      = coalesce(var.module_prefix, join(var.delimiter, compact([local.stage_prefix, var.application, module.azure_region.location_short, var.name])))
+
+  business_tags = {
+    namespace          = var.namespace
+    environment        = var.environment
+    environment_prefix = local.environment_prefix
+  }
+  technical_tags = {
+    stage      = var.stage
+    module     = var.name
+    repository = var.repository
+    region     = var.region
+  }
+  automation_tags = {
+    terraform_module = var.terraform_module
+    stage_prefix     = local.stage_prefix
+    module_prefix    = local.module_prefix
+  }
+  security_tags = {}
+
+  tags = merge(
+    local.business_tags,
+    local.technical_tags,
+    local.automation_tags,
+    local.security_tags,
+    var.tags
+  )
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Module Variables
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "lock_level" {
+  type        = string
+  default     = "CanNotDelete"
+  description = "The level of lock to apply to the resource group (e.g. `CanNotDelete`, `ReadOnly`)"
+}
+
