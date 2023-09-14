783e60498aa4ae43f359ffe6265ec3b891045b25
Benjamin Leon <benjamin.leon@gravicore.io>
2023-09-14 15:27:43 -0700

    GDEV-248 temp update 2

--- azure/rg/outputs.tf
+++ azure/rg/outputs.tf
@@ -8,12 +8,12 @@ output "resource_group_id" {
   value       = concat(azurerm_resource_group.default.*.id, [])[0]
 }
 
-output "state_storage_account_name" {
-  description = "The name of the Storage Account used for storing Terraform state."
-  value       = concat(azurerm_storage_account.tfstate.*.name, [])[0]
-}
+# output "state_storage_account_name" {
+#   description = "The name of the Storage Account used for storing Terraform state."
+#   value       = concat(azurerm_storage_account.tfstate.*.name, [])[0]
+# }
 
-output "state_storage_account_id" {
-  description = "The ID of the Storage Account used for storing Terraform state."
-  value       = concat(azurerm_storage_account.tfstate.*.id, [])[0]
-}
+# output "state_storage_account_id" {
+#   description = "The ID of the Storage Account used for storing Terraform state."
+#   value       = concat(azurerm_storage_account.tfstate.*.id, [])[0]
+# }
