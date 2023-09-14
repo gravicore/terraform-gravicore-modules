fbe7bdff85a639791b50abd8395e4cce9f2c981a
Benjamin Leon <benjamin.leon@gravicore.io>
2023-09-14 15:22:57 -0700

    GDEV-248 temporary update

--- azure/rg/main.tf
+++ azure/rg/main.tf
@@ -6,30 +6,30 @@ resource "azurerm_resource_group" "default" {
   name = local.stage_prefix
 }
 
-# Creates a storage account & storage container to store the Terraform state file(s) for this Resource Group.
-resource "azurerm_storage_account" "tfstate" {
-  count               = var.create ? 1 : 0
-  name                = join("", [var.namespace, var.environment, var.stage, "tf", "state"])
-  resource_group_name = concat(azurerm_resource_group.default.*.name, [""])[0]
-  location            = var.az_location
-  tags                = local.tags
+# # Creates a storage account & storage container to store the Terraform state file(s) for this Resource Group.
+# resource "azurerm_storage_account" "tfstate" {
+#   count               = var.create ? 1 : 0
+#   name                = join("", [var.namespace, var.environment, var.stage, "tf", "state"])
+#   resource_group_name = concat(azurerm_resource_group.default.*.name, [""])[0]
+#   location            = var.az_location
+#   tags                = local.tags
 
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
 
-  depends_on = [
-    azurerm_resource_group.default[0]
-  ]
-}
+#   depends_on = [
+#     azurerm_resource_group.default[0]
+#   ]
+# }
 
-resource "azurerm_storage_container" "tfstate" {
-  count = var.create ? 1 : 0
-  name  = join(var.delimiter, [var.namespace, var.environment, var.stage, "tf", "state"])
+# resource "azurerm_storage_container" "tfstate" {
+#   count = var.create ? 1 : 0
+#   name  = join(var.delimiter, [var.namespace, var.environment, var.stage, "tf", "state"])
 
-  storage_account_name  = concat(azurerm_storage_account.tfstate.*.name, [""])[0]
-  container_access_type = "blob"
+#   storage_account_name  = concat(azurerm_storage_account.tfstate.*.name, [""])[0]
+#   container_access_type = "blob"
 
-  depends_on = [
-    azurerm_storage_account.tfstate[0]
-  ]
-}
+#   depends_on = [
+#     azurerm_storage_account.tfstate[0]
+#   ]
+# }
