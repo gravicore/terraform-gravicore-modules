0a436fcd51010a27992387aeca469f2980a96fda
Kagan Mersin <kagan.mersin@gravicore.io>
2023-09-19 14:40:22 +0300

    add id nsg and snet outputs to the vnet

--- azure/vnet/outputs.tf
+++ azure/vnet/outputs.tf
@@ -2,10 +2,10 @@ output "vnet_id" {
   value = concat(azurerm_virtual_network.default.*.id, [""])[0]
 }
 
-output "subnets_info" {
-  value = { for s in azurerm_subnet.default : s.name => s }
+output "subnets_ids" {
+  value = { for s in azurerm_subnet.default : s => s.id }
 }
 
 output "subnet_nsg_details" {
-  value = { for s in azurerm_network_security_group.default : s.name => s }
+  value = { for s in azurerm_network_security_group.default : s => s.id }
 }
