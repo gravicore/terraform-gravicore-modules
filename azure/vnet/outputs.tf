5057a86fe27c12afb822013ef96350e9207333a8
Kagan Mersin <kagan.mersin@gravicore.io>
2023-09-19 11:47:05 +0300

    add new outputs based on keys on vnet

--- azure/vnet/outputs.tf
+++ azure/vnet/outputs.tf
@@ -2,19 +2,10 @@ output "vnet_id" {
   value = concat(azurerm_virtual_network.default.*.id, [""])[0]
 }
 
-output "vnet_details" {
-  value = azurerm_virtual_network.default
-}
-
-output "subnet_details" {
-  value = azurerm_subnet.default
+output "subnets_info" {
+  value = { for s in azurerm_subnet.default : s.name => s }
 }
 
 output "subnet_nsg_details" {
-  value = azurerm_network_security_group.default
+  value = { for s in azurerm_network_security_group.default : s.name => s }
 }
-
-output "subnet_nsg_association" {
-  value = azurerm_subnet_network_security_group_association.default
-}
-
