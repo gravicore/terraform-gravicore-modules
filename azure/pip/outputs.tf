a45a62f165429f700542a82b4f9aa35a3ae47885
Kagan Mersin <kagan.mersin@gravicore.io>
2023-09-24 19:32:04 +0300

    add id info to output

--- azure/pip/outputs.tf
+++ azure/pip/outputs.tf
@@ -1,4 +1,9 @@
 output "public_ip_mappings" {
-  value = { for k, pip in azurerm_public_ip.default : k => pip.ip_address }
-}
 
+  value = {
+    for key, resource in azurerm_public_ip.default  : key => {
+      name = resource.ip_address
+      id   = resource.id
+    }
+  }
+}
