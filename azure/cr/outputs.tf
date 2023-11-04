acb44f9be69103e3f00c813b026f03913c221b65
Kagan Mersin <kagan.mersin@gravicore.io>
2023-11-05 01:52:32 +0300

    GDEV-336 add public fqdn to output pf cr

--- azure/cr/outputs.tf
+++ azure/cr/outputs.tf
@@ -6,3 +6,7 @@ output "private_endpoint_fqdn" {
   value = concat(module.private_endpoint[*].private_endpoint_fqdn, [""])[0]
 }
 
+output "container_registry_public_fqdn" {
+  value = concat(azurerm_container_registry.default[*].login_server, [""])[0]
+}
+
