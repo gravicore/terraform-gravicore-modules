bbb3bc934ecbdf1b03afb76add2ec4787b582f64
Kagan Mersin <kagan.mersin@gravicore.io>
2023-11-04 09:28:34 +0300

    add fqdn output to psql

--- azure/psql/outputs.tf
+++ azure/psql/outputs.tf
@@ -1,6 +1,10 @@
 output "postgresql_flexible_server_id" {
   description = "The ID of the PostgreSQL Flexible Server."
   value       = concat(azurerm_postgresql_flexible_server.default.*.id, [""])[0]
+}
 
+output "postgresql_flexible_server_fqdn" {
+  description = "The FQDN of the PostgreSQL Flexible Server."
+  value       = concat(azurerm_postgresql_flexible_server.default.*.fqdn, [""])[0]
 }
 
