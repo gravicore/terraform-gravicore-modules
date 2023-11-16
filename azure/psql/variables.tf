fd9ba48951fbe9430f90f38f7d5e8e9a15c08b8d
Kagan Mersin <kagan.mersin@gravicore.io>
2023-11-16 18:50:51 +0300

    GDEV-336 map(bool) to map(any)

--- azure/psql/variables.tf
+++ azure/psql/variables.tf
@@ -252,7 +252,7 @@ variable "auto_grow_enabled" {
 
 variable "authentication" {
   description = "Map of authentication configuration."
-  type        = map(bool)
+  type        = map(any)
   default = {
     active_directory_auth_enabled = false
     password_auth_enabled         = true
