a1344f91b16ec4d4be6184a79d41d2dacaab28ab
Kagan Mersin <kagan.mersin@gravicore.io>
2023-12-16 19:58:27 +0300

    GDEV-336 fix outputs

--- azure/sqldb/outputs.tf
+++ azure/sqldb/outputs.tf
@@ -1,8 +1,8 @@
-output "resource_group_id" {
-  value = concat(azurerm_resource_group.default.*.id, [""])[0]
+output "mssql_database_ids" {
+  value = { for db in azurerm_mssql_database.single_database : db.name => db.id }
 }
 
-output "resource_group_name" {
-  value = concat(azurerm_resource_group.default.*.name, [""])[0]
+output "mssql_database_names" {
+  value = [for db in azurerm_mssql_database.single_database : db.name]
 }
 
