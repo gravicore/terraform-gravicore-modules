016c05cc1daec142a8a81ecfb0c416adc2d4ed71
Kagan Mersin <kagan.mersin@gravicore.io>
2024-01-07 09:05:49 +0300

    GDEV-340 fix duplication issues

--- azure/afd/variables.tf
+++ azure/afd/variables.tf
@@ -142,34 +142,13 @@ locals {
 # Module Variables
 # ----------------------------------------------------------------------------------------------------------------------
 
-#---------
-# Common
-variable "client_name" {
-  description = "Client name/account used in naming."
-  type        = string
-}
-
-variable "environment" {
-  description = "Project environment."
-  type        = string
-}
-
-variable "stack" {
-  description = "Project stack name."
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "Resource group name."
-  type        = string
-}
-
 # ------------------
 # CDN FrontDoor Profile
+
 variable "sku_name" {
   description = "Specifies the SKU for this CDN FrontDoor Profile. Possible values include `Standard_AzureFrontDoor` and `Premium_AzureFrontDoor`."
   type        = string
-  default     = "Standard_AzureFrontDoor"
+  default     = "Premium_AzureFrontDoor"
 }
 
 variable "response_timeout_seconds" {
