7385d702c13c6ec849b92d404e854962c7767d9a
Kagan Mersin <kagan.mersin@gravicore.io>
2024-02-08 16:30:26 +0300

    GDEV-342 add webtests_id outputs to appi

--- azure/appi/outputs.tf
+++ azure/appi/outputs.tf
@@ -7,3 +7,9 @@ output "component_id" {
   description = "The ID of this Application Insights component."
   value       = { for k, v in azurerm_application_insights.default : k => v.id }
 }
+
+output "webtest_ids" {
+  description = "The IDs of the webtests created for this Application Insights component."
+  value       = { for k, v in azapi_resource.webtests : k => v.id }
+}
+
