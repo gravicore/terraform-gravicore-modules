35c530a55a6794a94dc4bd7f07553df4caaf37b8
alisonchicoria <alison.chicoria@gravicore.io>
2024-04-17 14:28:53 -0300

    DEVOPS-2394 adding datasource

--- aws/lambda-appsync/appsync.tf
+++ aws/lambda-appsync/appsync.tf
@@ -22,9 +22,18 @@ variable "graphql_authentication_type" {
 # ----------------------------------------------------------------------------------------------------------------------
 # MODULES / RESOURCES
 # ----------------------------------------------------------------------------------------------------------------------
+resource "aws_appsync_datasource" "default" {
+  api_id           = aws_appsync_graphql_api.default.id
+  name             = "${var.module_prefix}-appsync-datasource"
+  service_role_arn = aws_iam_role.appsync_service_role.arn
+  type             = "AWS_LAMBDA"
 
+  lambda_config {
+    function_arn = aws_lambda_function.default.arn
+  }
+}
 
-resource "aws_appsync_graphql_api" "example" {
+resource "aws_appsync_graphql_api" "default" {
   authentication_type = var.graphql_authentication_type
   name                = "${var.module_prefix}-appsync-api"
   schema              = var.graphql_schema
