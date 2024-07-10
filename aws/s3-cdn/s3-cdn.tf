02f9500d72bca98a47d236f1533c3f471328f410
Benjamin Leon <benjamin.leon@gravicore.io>
2024-07-10 11:04:24 -0700

    GDEV-368 Added a custom_headers_config block

--- aws/s3-cdn/s3-cdn.tf
+++ aws/s3-cdn/s3-cdn.tf
@@ -434,6 +434,16 @@ variable "enable_content_security_policy" {
   description = "Enables the content security policy header"
 }
 
+variable "custom_headers" {
+  type = list(object({
+    header   = string
+    override = bool
+    value    = string
+  }))
+  default    = []
+  description = "List of custom headers to add to the response"
+}
+
 # ----------------------------------------------------------------------------------------------------------------------
 # MODULES / RESOURCES
 # ----------------------------------------------------------------------------------------------------------------------
@@ -508,6 +518,20 @@ resource "aws_cloudfront_response_headers_policy" "default" {
         content {
           content_security_policy = var.content_security_policy
           override                = var.content_security_policy_override
+        }
+      }
+    }
+  }
+
+  dynamic custom_headers_config {
+    for_each = length(var.custom_headers) > 0 ? [1] : []
+    content {
+      dynamic items {
+        for_each = var.custom_headers
+        content {
+          header   = items.value.header
+          override = items.value.override
+          value    = items.value.value
         }
       }
     }
