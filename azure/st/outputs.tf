41223f63d28e8a448575b9efad2857232b2a792b
Kagan Mersin <kagan.mersin@gravicore.io>
2024-08-07 00:43:42 +0300

    GDEV-363 add pe record set to the st  module

--- azure/st/outputs.tf
+++ azure/st/outputs.tf
@@ -3,3 +3,7 @@ output "storage_account_ids" {
   description = "The IDs of the Storage Accounts"
 }
 
+output "private_endpoint_record_sets" {
+  value       = { for k, v in module.private_endpoint : k => v.private_endpoint_record_sets }
+  description = "The Record Sets of the Private Endpoints"
+}
