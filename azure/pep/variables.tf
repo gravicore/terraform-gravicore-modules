c35f78fb71e359d924540b8a26d12c7b10df98ae
Kağan Mersin <kagan.mersin@gmail.com>
2024-10-09 14:04:35 +0300

    GDEV-381 fmt for modules

--- azure/pep/variables.tf
+++ azure/pep/variables.tf
@@ -207,7 +207,7 @@ variable "private_dns_zone_ids" {
 locals {
   resource_alias              = length(regexall("^([a-z0-9\\-]+)\\.([a-z0-9\\-]+)\\.([a-z]+)\\.(azure)\\.(privatelinkservice)$", var.target_resource)) == 1 ? var.target_resource : null
   resource_id                 = length(regexall("^\\/(subscriptions)\\/([a-z0-9\\-]+)\\/(resourceGroups)\\/([A-Za-z0-9\\-_]+)\\/(providers)\\/([A-Za-z\\.]+)\\/([A-Za-z]+)\\/([A-Za-z0-9\\-]+)", var.target_resource)) == 1 ? var.target_resource : null
-  is_not_private_link_service = local.resource_alias == null && ! contains(try(split("/", local.resource_id), []), "privateLinkServices")
+  is_not_private_link_service = local.resource_alias == null && !contains(try(split("/", local.resource_id), []), "privateLinkServices")
 
   private_dns_zone_group_name     = local.is_not_private_link_service ? join(var.delimiter, [local.module_prefix, var.az_region, "pdnszg"]) : null
   private_service_connection_name = join(var.delimiter, [local.module_prefix, var.az_region, "psc"])
