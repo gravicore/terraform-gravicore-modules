e0b826eb80c220dd274047dc87588abc1c8cf249
Kağan Mersin <kagan.mersin@gmail.com>
2025-01-11 16:42:01 +0300

    GDEV-381 wip

--- azure/sql/variables.tf
+++ azure/sql/variables.tf
@@ -275,8 +275,11 @@ variable "azuread_administrator" {
 
 variable "identity" {
   description = "Map of identity configuration."
-  type        = object()
-  default     = null
+  type = object({
+    type         = string
+    identity_ids = optional(list(string), null)
+  })
+  default = null
 }
 
 variable "primary_user_assigned_identity_id" {
