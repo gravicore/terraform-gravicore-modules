b2fca80f01d715388ce364c2d4131a51433d4276
celink-polkhovyk <pylyp.olkhovyk@celink.com>
2026-02-12 14:52:04 +0200

    DEVOPS-5495: Create Terraform module for standard EC2 SSM instance profile

--- aws/ec2/instance-profile/instance-profile.tf
+++ aws/ec2/instance-profile/instance-profile.tf
@@ -12,12 +12,6 @@ variable "ssm_policy_name" {
 # MODULES / RESOURCES
 # ----------------------------------------------------------------------------------------------------------------------
 
-resource "aws_iam_instance_profile" "ec2_profile" {
-  count = var.create ? 1 : 0
-  name  = local.module_prefix
-  role  = concat(aws_iam_role.role.*.name, [""])[0]
-}
-
 data "aws_iam_policy_document" "assume_role" {
   count = var.create ? 1 : 0
   statement {
@@ -39,6 +33,7 @@ resource "aws_iam_role" "ec2_role" {
   name               = local.module_prefix
   path               = "/"
   assume_role_policy = concat(data.aws_iam_policy_document.assume_role.*.json, [""])[0]
+  tags               = local.tags
 }
 
 resource "aws_iam_role_policy_attachment" "attach_policy" {
@@ -51,11 +46,21 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   count = var.create ? 1 : 0
   name  = local.module_prefix
   role  = concat(aws_iam_role.ec2_role.*.name, [""])[0]
+  tags  = local.tags
 }
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Outputs
 # ----------------------------------------------------------------------------------------------------------------------
+
+resource "aws_ssm_parameter" "instance_profile_name" {
+  count = var.create ? 1 : 0
+  name  = "/${local.stage_prefix}/instance-profile-name"
+  type  = "String"
+  value = concat(aws_iam_instance_profile.ec2_profile.*.name, [""])[0]
+
+  tags = local.tags
+}
 
 output "instance_profile_arn" {
   value = concat(aws_iam_instance_profile.ec2_profile.*.arn, [""])[0]
