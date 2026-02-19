892c4be6cfafe1c953a14da1558c8a7675bdd82b
Tyson Beck <Tyson.Beck@gravicore.io>
2026-02-19 09:07:51 -0700

    GDEV-433 updating cloudtrail resource to support data events
    updated terraform and provider version requirements
    updated access log bucket module version to resolve undefined bucket owner resource

--- aws/cloudtrail/module.tf
+++ aws/cloudtrail/module.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.3.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.26"
+      version = ">= 4.0"
     }
   }
 }
